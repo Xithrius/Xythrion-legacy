54f68b7c2b9d57d7ac7742a12be33deaf15f6100
Xithrius <15021300+Xithrius@users.noreply.github.com>
2019-03-16 18:24:26 -0700

    v0.01.0004: Bot works with multiple cogs in different files

---
+++ cogs/examples.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import commands as comms
+
+
+class ExampleCog(comms.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @comms.command(name='embeds')
+    @comms.guild_only()
+    async def example_embed(self, ctx):
+        """A simple command which showcases the use of embeds.
+
+        Have a play around and visit the Visualizer."""
+
+        embed = discord.Embed(title='Example Embed',
+                              description='Showcasing the use of Embeds...\nSee the visualizer for more info.',
+                              colour=0x98FB98)
+        embed.set_author(name='MysterialPy',
+                         url='https://gist.github.com/MysterialPy/public',
+                         icon_url='http://i.imgur.com/ko5A30P.png')
+        embed.set_image(url='https://cdn.discordapp.com/attachments/84319995256905728/252292324967710721/embed.png')
+
+        embed.add_field(name='Embed Visualizer', value='[Click Here!](https://leovoel.github.io/embed-visualizer/)')
+        embed.add_field(name='Command Invoker', value=ctx.author.mention)
+        embed.set_footer(text='Made in Python with discord.py@rewrite', icon_url='http://i.imgur.com/5BFecvA.png')
+
+        await ctx.send(content='**A simple Embed for discord.py@rewrite in cogs.**', embed=embed)
+
+def setup(bot):
+    bot.add_cog(ExampleCog(bot))
---
+++ credentials/README.md
@@ -0,0 +1,3 @@
+# Files:
+DStoken.txt: Token for discord bot
+.gitignore: Used if repository is made public
