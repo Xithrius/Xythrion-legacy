6836008af8c105a614a299ab3fa7d1e52287676b
Xithrius <15021300+Xithrius@users.noreply.github.com>
2019-03-28 17:08:06 -0700

    Fixed youtube-dl, now can play music through bot's mic

--- cogs/playback.py
+++ cogs/playback.py
@@ -1,6 +1,7 @@
 from discord.ext import commands as comms
 import discord
 import youtube_dl
+import asyncio
 
 from essentials.pathing import path, mkdir
 
@@ -16,19 +17,31 @@ class PlaybackCog(comms.Cog):
 
     @comms.command()
     @comms.is_owner()
-    async def download(self, ctx, url):
-        mkdir('audio', 'music')
-        ydl_opts = {
-            'outtmpl': f'{path()}/audio/music/%(title)s.%(ext)s',
-            'format': 'bestaudio/best',
-            'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '192',
-            }],
-        }
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([url])
+    async def play(self, ctx, url):
+        lock = asyncio.Lock()
+        await lock.acquire()
+        try:
+            mkdir('audio', 'music')
+            ydl_opts = {
+                'outtmpl': f'{path()}/audio/music/%(title)s.%(ext)s',
+                'format': 'bestaudio/best',
+                'postprocessors': [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3',
+                    'preferredquality': '200',
+                }],
+            }
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([url])
+                info_dict = ydl.extract_info(url, download=False)
+                video_title = info_dict.get('title', None)
+        finally:
+            lock.release()
+        vc = ctx.guild.voice_client
+        if not vc:
+            vc = await ctx.author.voice.channel.connect()
+        await ctx.send(f"playing '{video_title}'")
+        vc.play(discord.FFmpegPCMAudio(f'{path()}/audio/music/{video_title}.mp3'))
 
     @comms.command()
     @comms.is_owner()
@@ -41,38 +54,26 @@ class PlaybackCog(comms.Cog):
     @comms.command()
     @comms.is_owner()
     async def pause(self, ctx):
-        if await ctx.bot.is_owner(ctx.author):
-            vc = ctx.guild.voice_client
-            vc.pause()
-        else:
-            await ctx.send(f"{ctx.author.mention} you can't do this")
+        vc = ctx.guild.voice_client
+        vc.pause()
 
     @comms.command()
     @comms.is_owner()
     async def resume(self, ctx):
-        if await ctx.bot.is_owner(ctx.author):
-            vc = ctx.guild.voice_client
-            vc.resume()
-        else:
-            await ctx.send(f"{ctx.author.mention} you can't do this")
+        vc = ctx.guild.voice_client
+        vc.resume()
 
     @comms.command()
     @comms.is_owner()
     async def is_playing(self, ctx):
-        if await ctx.bot.is_owner(ctx.author):
-            vc = ctx.guild.voice_client
-            vc.is_playing()
-        else:
-            await ctx.send(f"{ctx.author.mention} you can't do this")
+        vc = ctx.guild.voice_client
+        vc.is_playing()
 
     @comms.command()
     @comms.is_owner()
     async def stop(self, ctx):
-        if await ctx.bot.is_owner(ctx.author):
-            vc = ctx.guild.voice_client
-            vc.stop()
-        else:
-            await ctx.send(f"{ctx.author.mention} you can't do this")
+        vc = ctx.guild.voice_client
+        vc.stop()
 
     @comms.command()
     @comms.is_owner()
