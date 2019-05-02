982edc416269bec7122f6e226d77d9b79fa12e12
Xithrius <15021300+Xithrius@users.noreply.github.com>
2019-05-02 16:13:33 -0700

    fixed cogs checking

--- bot.py
+++ bot.py
@@ -198,7 +198,7 @@ def setup_bot(bot=comms.Bot(command_prefix='$', case_insensitive=True)):
     # Searching for cogs within the cogs directory
     cogs = []
     for (dirpath, dirnames, filenames) in os.walk(path('cogs')):
-        cog = '.'.join(str(y) for y in dirpath[len(path()) + 1:].split('\\'))
+        cog = '.'.join(str(y) for y in dirpath[len(path()):].split('\\'))
         if '__pycache__' not in cog:
             cogs.extend([f'{cog}.{i[:-3]}' for i in filenames if i[:-3] not in [x[:-1] for x in open(path('relay', 'configuration', 'blocked_cogs.txt'), 'r').readlines()]])
     bot.add_cog(MainCog(bot, cogs))
