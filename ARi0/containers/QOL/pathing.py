033e73cb200d2ce5402306c111fbc8b470e81ebd
Xithrius <15021300+Xithrius@users.noreply.github.com>
2019-06-24 16:20:25 -0700

    Changed pathing

--- ARi0/containers/QOL/pathing.py
+++ ARi0/containers/QOL/pathing.py
@@ -16,6 +16,7 @@ import __main__
 
 from distutils import dir_util
 import platform
+import os
 
 
 """
@@ -29,16 +30,10 @@ def path(*objects):
     """
     Returns path relative to caller file location with additional objects, if any
     """
-    if platform.system() == 'Windows':
-        newPath = ((__main__.__file__).split("\\"))[:-1]
-        for i in objects:
-            newPath.append(i)
-        return '\\'.join(str(y) for y in newPath)
-    else:
-        newPath = ((__main__.__file__).split("/"))[:-1]
-        for i in objects:
-            newPath.append(i)
-        return '/'.join(str(y) for y in newPath)
+    newPath = ((__main__.__file__).split(os.sep))[:-1]
+    for i in objects:
+        newPath.append(i)
+    return (os.sep).join(str(y) for y in newPath)
 
 
 def create_dir(*objects):
