cba3f38e1a041dff220d5c3236cc23e6c997f930
Xithrius <15021300+Xithrius@users.noreply.github.com>
2019-08-05 03:22:13 -0700

    Re-created player from Danny (check desc)
    https://github.com/Rapptz/discord.py/blob/master/examples/basic_voice.py

--- cogs/playbackers/player.py
+++ cogs/playbackers/player.py
@@ -5,101 +5,114 @@
 """
 
 
-import youtube_dl
 import asyncio
-import os
+import youtube_dl
+import json
 
 from discord.ext import commands as comms
 import discord
 
-from handlers.modules.output import path, printc, now
+from handlers.modules.output import path
+
+# Suppress noise about console usage from errors
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+with open(path('handlers', 'configuration', 'config.json')) as f:
+    info = json.load(f)
+ytdl_format_options = info['ytdlopts']
+ffmpeg_options = info['ffmpeg_options']
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
 
 
-class Youtube_Playbacker(comms.Cog):
-    """ Playbacker for youtube """
+class YTDLSource(discord.PCMVolumeTransformer):
+
+    def __init__(self, source, *, data, volume=0.2):
+        super().__init__(source, volume)
+
+        self.data = data
+
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=True):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=False))
+
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+
+
+class Music(comms.Cog):
 
     def __init__(self, bot):
-        """ Object(s):
-        Bot
-        """
         self.bot = bot
-        self._loop = False
 
-    """ Checking permissions """
+    """ Checks """
+
+    @play.before_invoke
+    async def ensure_voice(self, ctx):
+        """ Makes sure the player is ready before connecting """
+        if ctx.voice_client is None:
+            if ctx.author.voice:
+                await ctx.author.voice.channel.connect()
+            else:
+                await ctx.send(f'{ctx.message.author.mention} You are not in a voice chat')
+        elif ctx.voice_client.is_playing():
+            await ctx.send(f"Stopping Audio to prioritize {ctx.message.author.mention}'s request")
+            ctx.voice_client.stop()
+
+    @volume.before_invoke
+    @leave.before_invoke
+    @pause.before_invoke
+    @resume.before_invoke
+    async def ensure_voice_modifier(self, ctx):
+        """ Checking if the voice client is connected """
+        if ctx.voice_client is None:
+            await ctx.send(f'Cannot preform the {ctx.command.name} action for a voice chat')
 
     async def cog_check(self, ctx):
-        return ctx.author.id in self.bot.config['owners']
-
-    """ """
-
-    def cog_unload(self):
-        """ """
-        try:
-            self.vc.stop()
-        except AttributeError:
-            pass
-        for audio in os.listdir(path('repository', 'music')):
-            os.remove(path('repository', 'music', audio))
-
-    """ Inputting/outputting YouTube """
-
-    async def dlyt(self, url):
-        with youtube_dl.YoutubeDL(self.bot.config['ytdlopts']) as ydl:
-            data = ydl.extract_info(url, download=True)
-        return data.get("id", None), data.get('title', None)
-
-    async def _play(self, video_id):
-        self.vc.play(discord.FFmpegPCMAudio(source=path('repository', 'music', f'{video_id}.mp3'), options='-loglevel fatal'))
-        self.vc.source = discord.PCMVolumeTransformer(self.vc.source)
-        self.vc.source.volume = 0.2
-        while self.vc.is_playing():
-            await asyncio.sleep(1)
-        self.vc.stop()
+        return ctx.message.author.id in self.bot.services[os.path.basename(__file__)[:-3]]
+        return True
 
     """ Commands """
 
     @comms.command()
-    async def play(self, ctx):
-        """ """
-        url = ctx.message.content[6:]
-        data = await self.dlyt(url)
-        video_id, video_title = data[0], data[1]
-        self.vc = ctx.guild.voice_client
-        if not self.vc:
-            self.vc = await ctx.author.voice.channel.connect()
-        await self._play(video_id)
-        os.remove(path('repository', 'music', f'{video_id}.mp3'))
+    async def play(self, ctx, url):
+        async with ctx.typing():
+                player = await YTDLSource.from_url(url, loop=self.bot.loop)
+                ctx.voice_client.play(player, after=lambda e: print(f'Player error: {e}') if e else None)
+        await ctx.send(f'Now playing: {player.title}')
+
+    @comms.command()
+    async def volume(self, ctx, volume: int):
+        ctx.voice_client.source.volume = volume / 100
+        await ctx.send(f"Changed volume to {volume}%")
+
+    @comms.command()
+    async def leave(self, ctx):
+        """ Leave a voice channel """
+        await ctx.voice_client.disconnect()
 
     @comms.command()
     async def pause(self, ctx):
-        """ """
-        try:
-            self.vc.pause()
-        except AttributeError:
-            await ctx.send("There's nothing to pause!")
+        """ Pauses audio """
+        await ctx.guild.voice_client.pause()
 
     @comms.command()
     async def resume(self, ctx):
-        try:
-            self.vc.resume()
-        except AttributeError:
-            await ctx.send("There's nothing to resume!")
+        """ Resumes audio """
+        await ctx.guild.voice_client.resume()
 
     @comms.command()
     async def stop(self, ctx):
-        try:
-            self.vc.stop()
-        except AttributeError:
-            await ctx.send("There's nothing to stop!")
-
-    @comms.command()
-    async def volume(self, ctx, volume):
-        try:
-            volume = int(volume * 100)
-            self.vc.source.volume = volume / 100
-        except AttributeError:
-            await ctx.send("There's no volume to adjust!")
+        """ Discontinues current audio from playing """
+        await ctx.voice_client.stop()
 
 
 def setup(bot):
-    bot.add_cog(Youtube_Playbacker(bot))
+    bot.add_cog(Music(bot))
