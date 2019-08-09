23c9cc72ab76b9944fe8e6d3f3b19c22c1ca43f0
Xithrius <15021300+Xithrius@users.noreply.github.com>
2019-08-09 04:49:25 -0700

    Rewrote Reddit cog

--- cogs/requesters/reddit.py
+++ cogs/requesters/reddit.py
@@ -23,83 +23,132 @@ class Reddit_Requester(comms.Cog):
     def __init__(self, bot):
         """ Object(s):
         Bot
+        Aiohttp session
+        Required headers for requests
         """
         self.bot = bot
+        self.s = aiohttp.ClientSession()
+        self.h = self.bot.services[os.path.basename(__file__)[:-3]]
 
-    """ Checks """
+    """ Cog events """
+
+    async def cog_unload(self):
+        self.bot.loop.create_tank(self.s.close())
+
+    """ Permission checking """
 
     async def cog_check(self, ctx):
-        return ctx.message.author.id in self.bot.services[os.path.basename(__file__)[:-3]]
+        """ """
+        _is_owner = ctx.message.author.id in self.bot.config['owners']
+        return all((_is_owner, self.h))
 
     """ Commands """
 
-    @comms.command(name='r/search')
-    async def search_subreddits(self, ctx, query):
-        info = await get_aiohttp('https://oauth.reddit.com/api/search_subreddits', self.bot.services['reddit'], {'query': query, 'include_over_18': True})
-        info = info['subreddits']
-        embed = discord.Embed(title=f'Reddit subreddit query for {query}', colour=0xc27c0e, timestamp=now())
-        for i in range(5):
-            try:
-                q_info = {'Link': f"https://www.reddit.com/r/{info[i]['name']}",
-                          'Subscribers': info[i]['subscriber_count'],
-                          'Active users': info[i]['active_user_count']}
-                q_info = '\n'.join(f'**{k}**: {v}' for k, v in q_info.items())
-                embed.add_field(name=f'__Query #{i + 1}__', value=q_info, inline=False)
-            except IndexError:
-                break
-        await ctx.send(embed=embed)
+    @comms.group()
+    async def reddit(self, ctx):
+        """ """
+        if ctx.invoked_subcommand is None:
+            pass
 
-    @comms.command(name='r/top')
-    async def topIn_subreddit(self, ctx, subreddit, top=5):
-        info = await get_aiohttp(f'https://oauth.reddit.com/r/{subreddit}/top', self.bot.services['reddit'], {'t': 'all', 'count': 1})
-        info = info['data']['children']
-        embed = discord.Embed(title=f'Top {top} posts of r/{subreddit}', colour=0xc27c0e, timestamp=now())
-        for i in range(top):
-            I = info[i]['data']
-            try:
-                q_info = {'Title': I['title'],
-                          'Link': f"https://www.reddit.com{I['permalink']}",
-                          'Upvotes': I['ups']}
-                q_info = '\n'.join(f'**{k}**: {v}' for k, v in q_info.items())
-                embed.add_field(name=f'__Top post #{i + 1}__', value=q_info, inline=False)
-            except IndexError:
-                break
-        await ctx.send(embed=embed)
+    @reddit.command()
+    async def _search(self, ctx, query):
+        """ Searches for a subreddit named similarly to <query>, returns top 5 results """
+        data = {'query': query, 'include_over_18': True}
+        async with self.s.get('https://oauth.reddit.com/api/search_subreddits', self.h, data) as r:
+            if r.status == 200:
+                _json = await r.json()
+                info = _json['subreddits']
+                embed = discord.Embed(title=f'Reddit subreddit query for {query}', colour=0xc27c0e, timestamp=now())
+                for i in range(5):
+                    try:
+                        q_info = {'Link': f"https://www.reddit.com/r/{info[i]['name']}",
+                                  'Subscribers': info[i]['subscriber_count'],
+                                  'Active users': info[i]['active_user_count']}
+                        q_info = '\n'.join(f'**{k}**: {v}' for k, v in q_info.items())
+                        embed.add_field(name=f'__Query #{i + 1}__', value=q_info, inline=False)
+                    except IndexError:
+                        break
+                await ctx.send(embed=embed, delete_after=60)
+            elif r.status == 404:
+                await ctx.send(f'Subreddit **{query}** could not be found.')
+            else:
+                await ctx.send(f'Requester failed. Status code: **{r.status}**')
 
-    @comms.command(name='r/preview')
-    async def previewIn_subreddit(self, ctx, subreddit):
-        info = await get_aiohttp(f'https://oauth.reddit.com/r/{subreddit}/top', self.bot.services['reddit'], {'t': 'all', 'limit': 100})
-        info = info['data']['children'][random.randint(1, 25)]['data']
-        embed = discord.Embed(title=f'Preview of the {subreddit} subreddit', colour=0xc27c0e, timestamp=now())
-        q_info = {'Title': info['title'], 'Link': f"https://www.reddit.com{info['permalink']}", 'Upvotes': info['ups']}
-        q_info = '\n'.join(f'**{k}**: {v}' for k, v in q_info.items())
-        embed.add_field(name='__**Info on post**__', value=q_info, inline=False)
-        embed.set_image(url=info['url'])
-        await ctx.send(embed=embed)
+    @reddit.command()
+    async def _top(self, ctx, subreddit, top=5):
+        """ Gives <top> links from the top of all time in <subreddit> """
+        data = {'t': 'all', 'count': 1}
+        async with self.s.get(f'https://oauth.reddit.com/r/{subreddit}/top', self.h, data) as r:
+            if r.status == 200:
+                _json = await r.json()
+                info = _json['data']['children']
+                embed = discord.Embed(title=f'Top {top} posts of r/{subreddit}', colour=0xc27c0e, timestamp=now())
+                for i in range(top):
+                    I = info[i]['data']
+                    try:
+                        q_info = {'Title': I['title'],
+                                  'Link': f"https://www.reddit.com{I['permalink']}",
+                                  'Upvotes': I['ups']}
+                        q_info = '\n'.join(f'**{k}**: {v}' for k, v in q_info.items())
+                        embed.add_field(name=f'__Top post #{i + 1}__', value=q_info, inline=False)
+                    except IndexError:
+                        break
+                await ctx.send(embed=embed, delete_after=60)
+            elif r.status == 404:
+                await ctx.send(f'Subreddit **{subreddit}** could not be found.')
+            else:
+                await ctx.send(f'Requester failed. Status code: **{r.status}**')
 
-    @comms.command(name='r/hot')
-    async def hotIn_subreddit(self, ctx, subreddit, amount=25):
-        info = await get_aiohttp(f'https://oauth.reddit.com/r/{subreddit}/hot', self.bot.services['reddit'], {'g': 'GLOBAL', 'count': amount})
-        info = info['data']['children']
-        for i in range(amount):
-            embed = discord.Embed(title=f'Currently hot on the {subreddit} subreddit', colour=0xc27c0e, timestamp=now())
-            I = info[random.randint(1, amount)]['data']
-            try:
-                if I['stickied']:
-                    raise KeyError
-                q_info = {'Link': f"https://www.reddit.com{I['permalink']}",
-                          'Upvotes': I['ups']}
+    @reddit.command()
+    async def _preview(self, ctx, subreddit):
+        """ Gives out a random post from the top 100 of all time in <subreddit> """
+        data = {'t': 'all', 'limit': 100}
+        async with self.s.get(f'https://oauth.reddit.com/r/{subreddit}/top', self.h, data) as r:
+            if r.status == 200:
+                _json = await r.json()
+                info = _json['data']['children'][random.randint(1, 25)]['data']
+                embed = discord.Embed(title=f'Preview of the {subreddit} subreddit', colour=0xc27c0e, timestamp=now())
+                q_info = {'Title': info['title'], 'Link': f"https://www.reddit.com{info['permalink']}", 'Upvotes': info['ups']}
                 q_info = '\n'.join(f'**{k}**: {v}' for k, v in q_info.items())
-                embed.add_field(name=f"__{I['title']}__", value=q_info, inline=False)
-                if not I['is_video']:
-                    embed.set_image(url=I['url'])
-                break
-            except KeyError:
-                continue
-        if I['is_video']:
-            await ctx.send(content=I['url'], embed=embed)
-        else:
-            await ctx.send(embed=embed)
+                embed.add_field(name='__**Info on post**__', value=q_info, inline=False)
+                embed.set_image(url=info['url'])
+                await ctx.send(embed=embed, delete_after=60)
+            elif r.status == 404:
+                await ctx.send(f'Subreddit **{subreddit}** could not be found.')
+            else:
+                await ctx.send(f'Requester failed. Status code: **{r.status}**')
+
+    @reddit.command()
+    async def _hot(self, ctx, subreddit. amount=10):
+        """ Gives <amount> of links from what's currently hot in <subreddit> """
+        data = {'g': 'GLOBAL', 'count': amount}
+        async with self.s.get(f'https://oauth.reddit.com/r/{subreddit}/hot', self.h, data) as r:
+            if r.status == 200:
+                _json = await r.json()
+                info = _json['data']['children']
+                for i in range(amount):
+                    embed = discord.Embed(title=f'Currently hot on the {subreddit} subreddit', colour=0xc27c0e, timestamp=now())
+                    I = info[random.randint(1, amount)]['data']
+                    try:
+                        if I['stickied']:
+                            raise KeyError
+                        q_info = {'Link': f"https://www.reddit.com{I['permalink']}",
+                                  'Upvotes': I['ups']}
+                        q_info = '\n'.join(f'**{k}**: {v}' for k, v in q_info.items())
+                        embed.add_field(name=f"__{I['title']}__", value=q_info, inline=False)
+                        if not I['is_video']:
+                            embed.set_image(url=I['url'])
+                        break
+                    except KeyError:
+                        continue
+                if I['is_video']:
+                    await ctx.send(content=I['url'], embed=embed)
+                else:
+                    await ctx.send(embed=embed, delete_after=60)
+            elif r.status == 404:
+                await ctx.send(f'Subreddit **{subreddit}** could not be found.')
+            else:
+                await ctx.send(f'Requester failed. Status code: **{r.status}**')
 
 
 def setup(bot):
