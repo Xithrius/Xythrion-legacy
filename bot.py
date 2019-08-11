818ed07cc63a8c68d640eba883ab254bec0d2e62
Xithrius <15021300+Xithrius@users.noreply.github.com>
2019-08-11 00:42:09 -0700

    Rewrote entire bot

--- bot.py
+++ bot.py
@@ -4,57 +4,63 @@
 > MIT license, Refer to LICENSE for more info
 """
 
-
 import collections
-import sqlite3
-import json
 import os
-import asyncio
+import json
+import sqlite3
 import aiohttp
+import asyncio
 
 from discord.ext import commands as comms
 import discord
 
-from handlers.modules.output import path, now, printc, create_table, progress_bar
+from handlers.modules.output import path, printc
 
 
-class Service_Connecter:
+class Service_Connector:
+    """ """
 
-    def __init__(self):
+    def __init__(self, config):
+        self.config = config
+        self.services = {k: False for k in self.config.services._fields}
 
+    async def start_services(self):
+        await self.attempt_weather()
+        await self.attempt_reddit()
+        await self.attempt_osu()
 
-    def attempt_weather(self):
-        f = self.tokens["weather"]
+    async def attempt_weather(self):
+        f = self.config.services.weather
         async with aiohttp.ClientSession() as session:
-            async with session.get(f'https://api.weatherbit.io/v2.0/forecast/daily?postal_code=12345&country=US&key={f}') as __t:
-                if __t.status == 200:
+            async with session.get(f'https://api.weatherbit.io/v2.0/forecast/daily?postal_code=12345&country=US&key={f}') as response:
+                if response.status == 200:
                     if not self.services['weather']:
                         printc('[ ! ]: WEATHER SERVICE AVAILABLE')
                     self.services['weather'] = True
                 else:
                     printc(f'WARNING: WEATHER SERVICE NOT AVAILABLE: {t.status}')
 
-    def attempt_reddit(self):
-        f = self.tokens['reddit']
-        client_auth = aiohttp.BasicAuth(login=f['ID'], password=f['secret'])
-        post_data = {"grant_type": "password", "username": f['username'], "password": f['password']}
-        headers = {"User-Agent": f"Xylene/{self.__version__} by {f['username']}"}
+    async def attempt_reddit(self):
+        f = self.config.services.reddit
+        client_auth = aiohttp.BasicAuth(login=f.id, password=f.secret)
+        post_data = {"grant_type": "password", "username": f.username, "password": f.password}
+        headers = {"User-Agent": f"Xylene/{self.__version__} by {f.username}"}
         async with aiohttp.ClientSession(auth=client_auth, headers=headers) as session:
-            async with session.post("https://www.reddit.com/api/v1/access_token", data=post_data) as __t:
-                if __t.status == 200:
-                    js = await __t.json()
+            async with session.post("https://www.reddit.com/api/v1/access_token", data=post_data) as response:
+                if response.status == 200:
+                    js = await response.json()
                     if not self.services['reddit']:
                         printc('[ ! ]: REDDIT SERVICE AVAILABLE')
                     self.services['reddit'] = {"Authorization": f"bearer {js['access_token']}", **headers}
                 else:
                     printc(f'WARNING: REDDIT SERVICE NOT AVAILABLE: {t.status}')
 
-    def attempt_osu(self):
-        f = self.tokens['osu']
+    async def attempt_osu(self):
+        f = self.config.services.osu
         parameters = {'k': f, 'u': 'Xithrius'}
         async with aiohttp.ClientSession() as session:
-            async with session.get(f'https://osu.ppy.sh/api/get_user', params=parameters) as __t:
-                if __t.status == 200:
+            async with session.get(f'https://osu.ppy.sh/api/get_user', params=parameters) as response:
+                if response.status == 200:
                     if not self.services['osu']:
                         printc('[ ! ]: OSU! SERVICE AVAILABLE')
                     self.services['osu'] = True
@@ -62,82 +68,75 @@ class Service_Connecter:
                     printc(f'WARNING: OSU! SERVICE NOT AVAILABLE: {t.status}')
 
 
-class Robot(comms.Bot):
+class Robot(comms.Bot, Service_Connector):
     """ """
 
     def __init__(self, *args, **kwargs):
         """ Objects:
-        Passing *args and **kwargs into comms.Bot
-        Creating background task for checking services
-        Creating requests database path
+
         """
         super().__init__(*args, **kwargs)
 
-        self.create_Attributes()
+        with open(path('handlers', 'configuration', 'config.json'), "r", encoding="utf8") as f:
+            data = json.dumps(json.load(f))
+
+        self.config = json.loads(data, object_hook=lambda d: collections.namedtuple("config", d.keys())(*d.values()))
+
+        self.__version__ = 'v0.0.1'
+
+        Service_Connector.__init__(self, config=self.config)
 
         self.background_services = self.loop.create_task(self.load_services())
 
-        self.db_path = path('repository', 'database', 'requests.db')
+        self.owner_ids = set(self.config.owners)
+        self.activity = discord.Activity(type=discord.ActivityType.watching, name='the users')
+
+        self.db_path = path('repository', 'data', 'requests.db')
 
         if not os.path.isfile(self.db_path):
-            self.create_Requests_db()
+            self.c = sqlite3.connect(self.db_path)
+            c = self.c.cursor()
+            # possibilities = ', '.join(str(y) for y in [f'{k} TEXT' for k, v in self.config.services.items()])
+            # c.execute('''CREATE TABLE Requests ()''')
+            # c.execute(''' ''')
+            self.c.close()
 
-    """ Preparing bot databases """
+        self.cog_folders = [folder for folder in os.listdir(path('cogs')) if folder != '__pycache__']
+        self.exts = []
+        for folder in self.cog_folders:
+            folder_cogs = [f'cogs.{folder}.{cog[:-3]}' for cog in os.listdir(path('cogs', folder)) if os.path.isfile(path('cogs', folder, cog))]
+            self.exts.extend(folder_cogs)
+        printc('[. . .]: LOADING EXTENSIONS')
+        for cog in self.exts:
+            try:
+                self.load_extension(cog)
+            except Exception as e:
+                printc(f'{cog}, {type(e).__name__}: {e}')
 
-    def create_Requests_db(self):
-        """ Creation of the requesting database if it does not exist """
-        possible_services = ', '.join(str(y) for y in [f'{k} TEXT' for k, v in self.services.items()])
-        self.c = sqlite3.connect(self.db_path)
-        c = self.c.cursor()
-        c.execute(f'''CREATE TABLE Requests (id INTEGER NOT NULL PRIMARY KEY UNIQUE, {possible_services})''')
-        self.c.commit()
-        self.c.close()
-
-    def create_Attributes(self):
-        """ """
-        with open(path('handlers', 'configuration', 'config.json'), "r") as f:
-            self.config = json.load(f)
-            self.services = {k: False for k in self.config['services'].keys()}
-            self.tokens = self.config['services']
-            self.__version__ = 'v0.0.1'
+    """ Background tasks """
 
     async def load_services(self):
-        await self.wait_until_ready()
+        # await self.wait_until_ready()
         while not self.is_closed():
-            await self.attempt_weather()
-            await self.attempt_reddit()
-            await self.attempt_osu()
+            await self.start_services()
             await asyncio.sleep(60)
 
     """ Events """
 
     async def on_ready(self):
-        """ Extensions are loaded as quickly as possible, main session is created """
         self.s = aiohttp.ClientSession()
-        printc('[. . .]: LOADING EXTENSION(S):')
-        extensions = {}
-        for folder in os.listdir(path('cogs')):
-            extensions[folder] = [cog for cog in os.listdir(path('cogs', folder)) if cog[:-3] not in ['__pycach', *self.config['blocked_cogs']]]
-        self.attached_extensions = []
-        for k, v in extensions.items():
-            self.attached_extensions.extend([f'cogs.{k}.{i[:-3]}' for i in v])
-        cog_amount = len(self.attached_extensions)
-        errors = []
-        progress_bar(0, cog_amount)
-        for i, cog in enumerate(self.attached_extensions):
-            try:
-                self.load_extension(cog)
-                progress_bar(i + 1, cog_amount)
-            except Exception as e:
-                errors.append(e)
-        if len(errors):
-            print('\n'.join(str(y) for y in errors))
-        create_table({'Cogs:': ['Extensions:.py'], **extensions})
-        await self.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name='the users'))
+        printc('[ ! ]: BOT IS READY FOR USE')
 
     async def close(self):
+        """ Safely closes connections """
         await self.s.close()
+        try:
+            self.c.close()
+        except Exception as e:
+            pass
         await super().close()
+
+    """ Miscellaneous events """
 
     async def on_disconnect(self):
         """ Sends warning when the client disconnects from the network """
@@ -145,7 +144,8 @@ class Robot(comms.Bot):
 
     async def on_connect(self):
         """ Sends warning when the client connects to the network """
-        printc('[WARNING]: CLIENT HAS CONNECTED TO NETWORK')
+        # printc('[WARNING]: CLIENT HAS CONNECTED TO NETWORK')
+        pass
 
     async def on_resumed(self):
         """ Sends warning when the client resumes a session """
@@ -157,95 +157,41 @@ class MainCog(comms.Cog):
 
     def __init__(self, bot):
         """ Objects:
-        Robot(comms.Bot) as a class attribute
+
         """
         self.bot = bot
 
-    """ Checks """
+    """ Cog checks """
 
     async def cog_check(self, ctx):
-        return ctx.author.id in self.bot.config['owners']
+        return ctx.author.id in self.bot.owner_ids
 
     """ Commands """
 
+    @comms.command()
+    async def exit(self, ctx):
+        """ Makes the bot log out """
+        printc('[WARNING]: BOT IS LOGGING OUT')
+        await ctx.bot.logout()
+
     @comms.command(name='r')
-    async def reload_cog(self, ctx):
-        broken_cogs = []
-        for cog in self.bot.attached_extensions:
+    async def reload(self, ctx):
+        for cog in self.bot.exts:
             try:
                 self.bot.unload_extension(cog)
                 self.bot.load_extension(cog)
             except discord.ext.commands.errors.ExtensionNotFound:
-                self.bot.attached_extensions.remove(cog)
-            except Exception as e:
-                broken_cogs.append([cog, e])
-        if not len(broken_cogs):
-            await ctx.send(f'**{len(self.bot.attached_extensions)}** cog(s) have been reloaded')
-        else:
-            info = '\n'.join(f'[{y[0]}]: {type(y[1]).__name__} - #{y[1]}' for y in broken_cogs)
-            await ctx.send(f'''```css\n{info}```''')
+                self.bot.exts.remove(cog)
+        printc('[ ! ]: COGS HAVE BEEN RELOADED')
 
-    @comms.command(name='l')
-    async def unload_cog(self, ctx, cog: str):
-        try:
-            self.bot.load_extension(cog)
-        except Exception as e:
-            await ctx.send(f'Extension {cog.split(".")[-1]} could not be loaded')
+    """ Events """
 
-    @comms.command(name='u')
-    async def load_cog(self, ctx, cog: str):
-        """ Unloads a specific cog into the bot"""
-        try:
-            self.bot.unload_extension(cog)
-            self.bot.attached_extensions.remove(cog)
-        except Exception as e:
-            await ctx.send(f'Extension {cog.split(".")[-1]} could not be unloaded')
-
-    @comms.command(name='lst_cogs')
-    async def list_all_cogs(self, ctx):
-        """ Lists all current cogs loaded into the bot """
-        await ctx.send(f'List of **{len(self.bot.attached_extensions)}** cog(s): {", ".join(y.split(".")[-1] for y in self.bot.attached_extensions)}')
-
-    @comms.command()
-    async def exit(self, ctx):
-        """ Makes the client logout """
-        printc('[WARNING]: CLIENT IS LOGGING OUT')
-        for cog in self.bot.attached_extensions:
-            try:
-                self.bot.unload_extension(cog)
-            except Exception as e:
-                await ctx.send(f'Extension {cog.split(".")[-1]} could not be unloaded')
-        try:
-            self.c.close()
-        except AttributeError:
-            pass
-        await ctx.bot.logout()
+    @comms.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        await ctx.send(f'{ctx.message.author.mention} has caused error: `{error}`')
 
 
 if __name__ == "__main__":
     bot = Robot(command_prefix=comms.when_mentioned_or('.'), help_command=None)
     bot.add_cog(MainCog(bot))
-    bot.run(bot.config['discord'], bot=True, reconnect=True)
-
-'''
-@comms.Cog.listener()
-async def on_command_error(self, ctx, error):
-    if isinstance(error, discord.ext.commands.errors.CommandInvokeError):
-        pass
-    elif isinstance(error, discord.ext.commands.errors.CheckFailure):
-        await ctx.send(f'You do not have enough permissions to run the command **.{ctx.command.name}**')
-    elif isinstance(error, discord.ext.commands.CommandNotFound):
-        msg = ctx.message.content
-        try:
-            msg = msg[:msg.index(' ')]
-        except ValueError:
-            pass
-        possibilities = [x.name for x in self.bot.commands]
-        if len(possibilities):
-            embed = discord.Embed(title='\n'.join(str(y) for y in [x.name for x in self.bot.commands] if y in msg))
-            await ctx.send(content=f'**{msg}** command not found. Maybe you meant one of the following?', embed=embed)
-        else:
-            await ctx.send(f'Could not find a command similar to **{msg}**')
-    else:
-        await ctx.send(f'Notifying owner <@{self.bot.owner_id}> of error `{error}`')
-'''
+    bot.run(bot.config.discord, bot=True, reconnect=True)
