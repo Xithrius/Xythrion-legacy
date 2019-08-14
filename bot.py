5633996d0460d95b468c79f57ecbca849bd61d8d
Xithrius <15021300+Xithrius@users.noreply.github.com>
2019-08-13 22:17:44 -0700

    Added logging

--- bot.py
+++ bot.py
@@ -10,11 +10,19 @@ import json
 import sqlite3
 import aiohttp
 import asyncio
+import logging
 
 from discord.ext import commands as comms
 import discord
 
 from handlers.modules.output import path, printc
+
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename=path('repository', 'logs', 'discord.log'), encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 
 class Service_Connector:
