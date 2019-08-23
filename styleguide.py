5ffd39a7cfdf374857f10d872acbd03d16ac61a0
Xithrius <15021300+Xithrius@users.noreply.github.com>
2019-08-23 12:28:05 -0700

    Modified styleguide

--- styleguide.py
+++ styleguide.py
@@ -245,9 +245,6 @@ class ExampleClass:
     def example_method(self, param1, param2):
         """Class methods are similar to regular functions.
 
-        Note:
-            Do not include the `self` parameter in the ``Args`` section.
-
         Args:
             param1: The first parameter.
             param2: The second parameter.
