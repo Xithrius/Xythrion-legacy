900173cad033bd4e5cc5e42ac79889a2b9cfa7eb
Xithrius <15021300+Xithrius@users.noreply.github.com>
2019-08-31 11:11:41 -0700

    Starting to rewrite bot

--- bot.py
+++ bot.py
@@ -39,6 +39,11 @@ handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(me
 logger.addHandler(handler)
 
 
+class MyHelpCommand(commands.MinimalHelpCommand):
+    def get_command_signature(self, command):
+        return '{0.clean_prefix}{1.qualified_name} {1.signature}'.format(self, command)
+
+
 class Robot(comms.Bot):
     """Subclassing comms.Bot to setup up all class attributes and services before cogs are loaded."""
 
@@ -229,6 +234,20 @@ class MainCog(comms.Cog):
 
         #: Setting Robot(comms.Bot) as a class attribute
         self.bot = bot
+
+        #: Creating help command
+        self._original_help_command = bot.help_command
+        bot.help_command = MyHelpCommand()
+        bot.help_command.cog = self
+
+    def cog_unload(self):
+        """Recreates help command if cog is unloaded
+
+        Returns:
+            A set help command depending on what's set in the class
+
+        """
+        self.bot.help_command = self._original_help_command
 
     async def cog_check(self, ctx):
         """Checks permissions of a user.
