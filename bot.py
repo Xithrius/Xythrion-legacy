4ba68eddfdb2877acbf24d6302142f827b717183
Xithrius <15021300+Xithrius@users.noreply.github.com>
2019-09-06 06:13:14 -0700

    Commands invite, about, and website added

--- bot.py
+++ bot.py
@@ -54,8 +54,46 @@ class RobotCog(comms.Cog):
 
     @comms.command()
     async def exit(self, ctx):
-        ds.w('[ Warning ]: Logging out.')
+        ds.w('Logging out.')
         await ctx.bot.logout()
+
+
+class InfoCog(comms.Cog):
+    """."""
+
+    def __init__(self, bot):
+        #:
+        self.bot = bot
+
+    @comms.command()
+    async def invite(self, ctx):
+        """Gives the invite link of this bot. It is not 'essential', but it's still useful.
+        Args:
+            ctx: Context object where the command is called.
+        Returns:
+            The invite link so the bot can be invited to a server.
+        """
+        await ctx.send(f'https://discordapp.com/oauth2/authorize?client_id={self.bot.user.id}&scope=bot&permissions=32885952')
+
+    @comms.command()
+    async def about(self, ctx):
+        """Returns information about this bot's origin
+        Args:
+            ctx: Context object where the command is called.
+        Returns:
+            An embed object with links to creator's information and bot's repository.
+        """
+        info = {
+            'Twitter': 'https://twitter.com/_Xithrius',
+            'Github': 'https://github.com/Xithrius/Xythrion'
+        }
+        embed = discord.Embed(title='Project creation date: March 30, 2019', description='\n'.join(f'[`{k}`]({v})' for k, v in info.items()), colour=self.bot.ec)
+        await ctx.send(embed=embed)
+
+    @comms.command()
+    async def website(self, ctx):
+        """ """
+        embed = discord.Embed(description='`https://xithrius.github.io/Xythrion/`')
 
 
 if __name__ == "__main__":
