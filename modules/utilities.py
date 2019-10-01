7744888f57c39c3521c3df4f7fc81d4e6da687c7
Xithrius <15021300+Xithrius@users.noreply.github.com>
2019-10-01 09:31:50 -0700

    Utilities for discord QoL

---
+++ modules/utilities.py
@@ -0,0 +1,19 @@
+"""
+>> Xythrion
+> Copyright (c) 2019 Xithrius
+> MIT license, Refer to LICENSE for more info
+"""
+
+
+import discord
+
+
+class d_embed:
+
+    @classmethod
+    async def block_image(cls, string, image_url):
+        pass
+
+    @classmethod
+    async def desc_list(self, lst):
+        pass
