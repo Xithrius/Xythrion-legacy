979a571e6abf07adb7624b919ebd05c29e34e855
Xithrius <15021300+Xithrius@users.noreply.github.com>
2019-10-06 02:16:33 -0700

    Added level refreshing/creator

--- cogs/recorders/messages.py
+++ cogs/recorders/messages.py
@@ -5,9 +5,12 @@
 """
 
 
-import sqlite3
+import asyncpg
+from collections import defaultdict
+import json
 
 from discord.ext import commands as comms
+import discord
 
 from modules.output import path
 
@@ -22,9 +25,37 @@ class Message_Recorder(comms.Cog):
 
     """ Commands """
 
-    @comms.command()
-    async def rank(self, ctx):
-        pass
+    @comms.command(enabled=False, hidden=True)
+    @comms.is_owner()
+    async def rl(self, ctx):
+        """ONLY USE THIS COMMAND IF DATABASE IS DELETED."""
+        self.messages = []
+        for guild in self.bot.guilds:
+            for channel in guild.text_channels:
+                try:
+                    messages = await channel.history(limit=None).flatten()
+                    self.messages.extend(messages)
+                except discord.errors.Forbidden:
+                    pass
+        self.user_records = {}
+        for message in self.messages:
+            if message.author.id not in self.user_records.keys():
+                self.user_records[message.author.id] = [0, 0, 0, 0]
+            y = self.user_records[message.author.id]
+            if message.attachments:
+                for f in message.attachments:
+                    f = f.filename[-4:]
+                    if f in ['.jpg', '.png']:
+                        y[1] += 1
+                    elif f in ['.gif', '.mp4']:
+                        y[2] += 1
+                    elif f in ['.mp3', '.flv']:
+                        y[3] += 1
+            else:
+                y[0] += 1
+            self.user_records[message.author.id] = y
+        for user_id, records in self.user_records.items():
+            await self.bot.conn.execute('''INSERT INTO Messages(identification, messages, images, videos, audios) VALUES($1, $2, $3, $4, $5)''', user_id, *records)
 
 
 def setup(bot):
