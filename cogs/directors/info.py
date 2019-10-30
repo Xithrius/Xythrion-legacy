58e8ef16a98594f48c1ec391937d99a6abbc6f6d
Xithrius <15021300+Xithrius@users.noreply.github.com>
2019-10-30 01:39:03 -0700

    removing whitespaces

--- cogs/directors/info.py
+++ cogs/directors/info.py
@@ -20,37 +20,26 @@ class Info_Director(comms.Cog):
     @comms.command()
     async def invite(self, ctx):
         """Gives the invite link of this bot. It is not 'essential', but it's still useful.
-        
+
         Returns:
             The invite link so the bot can be invited to a server.
-        
+
         """
         await ctx.send(f'https://discordapp.com/oauth2/authorize?client_id={self.bot.user.id}&scope=bot&permissions=32885952')
 
     @comms.command()
     async def about(self, ctx):
         """Returns information about this bot's origin
-        
+
         Returns:
             An embed object with links to creator's information and bot's repository.
-        
+
         """
         info = {
             'Twitter': 'https://twitter.com/_Xithrius',
             'Github': 'https://github.com/Xithrius/Xythrion'
         }
         embed = discord.Embed(title='Project creation date: March 30, 2019', description='\n'.join(f'[`{k}`]({v})' for k, v in info.items()))
-        await ctx.send(embed=embed)
-
-    @comms.command()
-    async def website(self, ctx):
-        """Returns website for the bot (this replaced the README).
-
-        Returns:
-            An embed object containing the website link for the bot.
-
-        """
-        embed = discord.Embed(description='`https://github.com/Xithrius/Xythrion`')
         await ctx.send(embed=embed)
 
 
