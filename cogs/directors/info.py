1a00281a94e6f283e78de11670f38eb053f3c833
Xithrius <15021300+Xithrius@users.noreply.github.com>
2019-10-31 04:02:17 -0700

    Updated formatting for links

--- cogs/directors/info.py
+++ cogs/directors/info.py
@@ -19,27 +19,34 @@ class Info_Director(comms.Cog):
 
     @comms.command()
     async def invite(self, ctx):
-        """Gives the invite link of this bot. It is not 'essential', but it's still useful.
+        """Gives the invite link of this bot.
 
         Returns:
             The invite link so the bot can be invited to a server.
 
         """
-        await ctx.send(f'https://discordapp.com/oauth2/authorize?client_id={self.bot.user.id}&scope=bot&permissions=32885952')
+        info = [
+            'https://discordapp.com/oauth2/authorize?client_id=',
+            self.bot.user.id,
+            '&scope=bot&permissions=32885952'
+        ]
+        await ctx.send(''.join(str(y) for y in info))
 
     @comms.command()
     async def about(self, ctx):
         """Returns information about this bot's origin
 
         Returns:
-            An embed object with links to creator's information and bot's repository.
+            An embed object with links to creator's information.
 
         """
         info = {
             'Twitter': 'https://twitter.com/_Xithrius',
             'Github': 'https://github.com/Xithrius/Xythrion'
         }
-        embed = discord.Embed(title='Project creation date: March 30, 2019', description='\n'.join(f'[`{k}`]({v})' for k, v in info.items()))
+        embed = discord.Embed(title='Project creation date: March 30, 2019',
+                              description='\n'.join(
+                                  f'[`{k}`]({v})' for k, v in info.items()))
         await ctx.send(embed=embed)
 
 
