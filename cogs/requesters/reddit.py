5fe0a4a1d9462412a0c5fe84897a0cefe959a16d
Xithrius <xithrius@gmail.com>
2020-02-14 02:38:33 -0800

    Finished Reddit cog for now

--- cogs/requesters/reddit.py
+++ cogs/requesters/reddit.py
@@ -7,10 +7,13 @@
 
 import aiohttp
 import random
+import typing
 
 import discord
 from discord.ext import commands as comms
 from discord.ext.commands.cooldowns import BucketType
+
+from modules.output import shorten
 
 
 class Reddit(comms.Cog):
@@ -21,36 +24,41 @@ class Reddit(comms.Cog):
         self.bot = bot
 
     @comms.command(aliases=['sub', 'subreddit'])
-    async def reddit(self, ctx, subreddit, status='hot', timeframe='day', posts: int=1):
+    async def reddit(self, ctx, subreddit, status='hot', timeframe='day', amount: typing.Optional[int]=1):
         """ """
         status, timeframe = status.lower(), timeframe.lower()
         statuses = ['top', 'hot', 'controvertial', 'new', 'guilded']
         timeframes = ['hour', 'day', 'week', 'month', 'year', 'all']
 
-        if int(posts) not in range(0, 20):
-            return await ctx.send(f'Please pick a number of posts between 0 and 100')
+        if int(amount) not in range(1, 11):
+            return await ctx.send(f'Please pick an amount of posts between 1 and 10')
         if status not in statuses:
             return await ctx.send(f'Please pick a status within {", ".join(str(y) for y in status)}')
         if timeframe not in timeframes:
-            return await ctx.send(f'Please pick a status within {", ".join(str(y) for y in timeframes)}')
+            return await ctx.send(f'Please pick a timeframe within {", ".join(str(y) for y in timeframes)}')
         
         url = f'https://reddit.com/r/{subreddit}/{status}.json?limit=100&t={timeframe}'
         async with self.bot.session.get(url) as r:
             assert r.status == 200
             js = await r.json()
             js = js['data']['children']
-            try:
-                if posts == 1:
-                    post = js[random.randint(0, len(js))]['data']
-                else:
-                    pick = random.randint(0, len(js)) - posts
-                    post = [post['data'] for post in js[pick:pick + posts]]
-                lst = [(f"https://reddit.com{item['permalink']}", item['title']) for item in post]
-                # NOTE: Make cut down function to shorten titles.
-                embed = discord.Embed(description='\n'.join(f'[`{y[0]}`]({y[1]})' for y in lst))
-            except IndexError:
-                return await ctx.send(f'This subreddit does not have enough posts.')
+            single = False
 
+            if amount == 1:
+                post = [js[random.randint(0, len(js))]['data']]
+                if post[0]['url'][-4:] in ('.jpg', 'jpeg', '.png'):
+                    single = post[0]['url'], post[0]['ups'], post[0]['author']
+            else:
+                pick = random.randint(0, len(js)) - amount
+                post = [post['data'] for post in js[pick:pick + amount]]
+            
+            lst = [(shorten(item['title']), f"https://reddit.com{item['permalink']}") for item in post]
+            embed = discord.Embed(title=f'*r/{subreddit}*',
+                                  description='\n'.join(f'[`{y[0]}`]({y[1]})' for y in lst))
+            if single:
+                embed.set_image(url=single[0])
+                embed.set_footer(text=f'Upvotes: {single[1]}\nAuthor: u/{single[2]}')
+            await ctx.send(embed=embed)
 
 
 def setup(bot):
