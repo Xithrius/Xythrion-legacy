fe7dd797b70041f658939d156d4b2501e373e851
Xithrius <xithrius@gmail.com>
2020-02-26 14:38:40 -0800

    Moved links to main bot file

--- bot.py
+++ bot.py
@@ -144,7 +144,7 @@ class Xythrion(comms.Bot):
 
 
 class Main_Cog(comms.Cog):
-    """Cog required for development and control."""
+    """Cog required for development and control, along with some extras."""
 
     def __init__(self, bot):
         self.bot = bot
@@ -184,6 +184,36 @@ class Main_Cog(comms.Cog):
             pass
         Status('Logging out...', 'warn')
         await ctx.bot.logout()
+
+    @comms.command()
+    async def invite(self, ctx):
+        """Gives the invite link of this bot.
+
+        Returns:
+            The invite link so the bot can be invited to a server.
+
+        """
+        url = f'https://discordapp.com/oauth2/authorize?client_id={self.bot.user.id}&scope=bot&permissions=37604544'
+        embed = discord.Embed(description=f'[`Xythrion invite url`]({url})')
+        await ctx.send(embed=embed)
+
+    @comms.command()
+    async def info(self, ctx):
+        """Returns information about this bot's origin
+
+        Returns:
+            An embed object with links to creator's information.
+
+        """
+        d = abs(datetime.datetime(year=2019, month=3, day=13) - datetime.datetime.now()).days
+        info = {
+            f'Project created {d} days ago, on March 13, 2019': 'https://github.com/Xithrius/Xythrion/tree/55fe604d293e42240905e706421241279caf029e',
+            'Xythrion Github repository': 'https://github.com/Xithrius/Xythrion',
+            "Xithrius' Twitter": 'https://twitter.com/_Xithrius',
+            "Xithrius' Github": 'https://github.com/Xithrius'
+        }
+        embed = discord.Embed(description='\n'.join(f'[`{k}`]({v})' for k, v in info.items()))
+        await ctx.send(embed=embed)
 
 
 if __name__ == "__main__":
