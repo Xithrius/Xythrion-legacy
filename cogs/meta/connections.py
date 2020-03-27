74301c5100bb1b7798972e3c57caaafc300a5617
Xithrius <xithrius@gmail.com>
2020-03-26 18:34:48 -0700

    Created cog for testing connections

---
+++ cogs/meta/connections.py
@@ -0,0 +1,52 @@
+"""
+> Xythrion
+> Copyright (c) 2020 Xithrius
+> MIT license, Refer to LICENSE for more info
+"""
+
+
+import time
+from http.client import responses
+
+from discord.ext import commands as comms
+
+
+class Connections(comms.Cog):
+    """Summary for Connections
+
+    Attributes:
+        bot (:obj:`comms.Bot`): Represents a Discord bot.
+
+    """
+
+    def __init__(self, bot):
+        """Creating important attributes for this class.
+
+        Args:
+            bot (:obj:`comms.Bot`): Represents a Discord bot.
+
+        """
+        self.bot = bot
+
+    @comms.command(aliases=['status', 'response', 'code'])
+    async def http_code(self, ctx, code: int):
+        """Gets the http status code information.
+
+        Args:
+            ctx (:obj:`comms.Context`): Represents the context in which a command is being invoked under.
+            code (int): The status code that is wanted.
+
+        """
+        await ctx.send(f'{code} means {responses[int(code)]}')
+
+    @comms.command()
+    async def ping(self, ctx):
+        timeStart = time.time()
+        # await ctx.trigger_typing()
+        timeEnd = time.time()
+        timeTaken = timeEnd - timeStart
+        await ctx.send(f'How long it takes for this bot to respond: `{timeTaken}ms`')
+
+
+def setup(bot):
+    bot.add_cog(Connections(bot))
