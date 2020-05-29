08273819d03defe391d44cbbec1e55a184c97320
Xithrius <xithrius@gmail.com>
2020-05-29 05:21:50 -0700

    Gutted since parsing will be located elsewhere.

--- cogs/generation/graphing.py
+++ cogs/generation/graphing.py
@@ -5,40 +5,19 @@
 """
 
 
-import os
-import re
-import typing as t
-import traceback
-import sys
-
-import discord
-import numpy as np
 from discord.ext import commands as comms
 from discord.ext.commands.cooldowns import BucketType
 
-from utils import (
-    embed_attachment, get_filename, path, codeblock, parallel_executor
-)
-
-
-try:
-    import matplotlib
-    matplotlib.use('Agg')
-    import matplotlib.pyplot as plt
-    plt.style.use('dark_background')
-except Exception as e:
-    traceback.print_exception(type(e), e, e.__traceback__, file=sys.stderr)
-
 
 class Graphing(comms.Cog):
-    """Summary for Graphing
+    """Parsing a user's input and making a graph out of it..
 
     Attributes:
         bot (:obj:`comms.Bot`): Represents a Discord bot.
 
     """
 
-    def __init__(self, bot):
+    def __init__(self, bot: comms.Bot):
         """Creating important attributes for this class.
 
         Args:
@@ -47,92 +26,26 @@ class Graphing(comms.Cog):
         """
         self.bot = bot
 
-    """ Cog-specific functions """
-
-    @parallel_executor
-    def parse_equation(self, eq: str, domain: list = None) -> t.Tuple[np.array]:
-        eq = re.finditer(r'([\-\+])*(\s)*(\w)*(\^([\-\+])*\d)*', eq)
-        eq = [y.group().replace(' ', '').split('^') for y in eq if not y.group().strip() == '']
-
-        x = np.arange(*domain if domain else [-10, 10], 0.1)
-        y = x
-
-        for item in eq:
-            if 'x' not in item[0]:
-                y += int(item[0])
-
-            else:
-                try:
-                    y *= int(item[0][:-1])
-                except (IndexError, ValueError):
-                    pass
-
-                try:
-                    y = pow(y, float(item[1]))
-                except IndexError:
-                    pass
-
-        return x, y
-
-    @parallel_executor
-    def create_plot(self, lst: t.List[tuple]) -> str:
-        plt.clf()
-
-        for item in lst:
-            x, y = item
-            plt.plot(x, y)
-
-        f = f'{get_filename()}.png'
-        plt.savefig(path('tmp', f), format='png')
-
-        return f
-
     """ Commands """
 
     @comms.cooldown(1, 5, BucketType.user)
-    @comms.command()
-    async def graph(self, ctx, *, entry: str):
+    @comms.command(enabled=False)
+    async def graph(self, ctx: comms.Context, *, entry: str) -> None:
         """Graphing equations.
 
         Args:
             ctx (:obj:`comms.Context`): Represents the context in which a command is being invoked under.
             eq (str): The equation to be parsed and graphed.
 
+        Returns:
+            bool: Always None.
+
         Command examples:
             >>> [prefix]graph x^2 + x
 
         """
-        domain = None
-
-        try:
-            domain = entry[entry.index('domain') + len('domain') + 1:]
-            domain = [int(x.strip()) for x in domain.split(',')]
-            E = entry[:entry.index('domain') - 1].strip()
-        except ValueError:
-            E = entry
-
-        eq = [x.strip() for x in E.split(',')]
-
-        lst = []
-
-        for item in eq:
-            try:
-                x, y = await self.parse_equation(item, domain)
-                lst.append((x, y))
-
-            except (ValueError, IndexError):
-                return await ctx.send(
-                    '`The graph command only accepts polynomials with exponents and coefficients.`')
-                break
-
-        f = await self.create_plot(lst)
-
-        embed = discord.Embed()
-        file, embed = embed_attachment(path('tmp', f), embed)
-        embed.description = await quick_block({'Equation(s)': E, 'Domain': domain})
-
-        await ctx.send(file=file, embed=embed)
-        os.remove(path('tmp', f))
+        # NOTE: Will be filled in once parsing utility is finished.
+        pass
 
 
 def setup(bot):
