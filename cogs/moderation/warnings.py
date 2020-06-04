4d3b738dc29257e6164f69f042052024014aad86
Xithrius <xithrius@gmail.com>
2020-06-03 19:38:27 -0700

    Added report checking command

--- cogs/moderation/warnings.py
+++ cogs/moderation/warnings.py
@@ -7,7 +7,10 @@
 
 from datetime import datetime
 
+import discord
 from discord.ext import commands as comms
+
+from utils import fancy_embed, markdown_link, tracebacker, wait_for_reaction
 
 
 class Warnings(comms.Cog):
@@ -26,6 +29,18 @@ class Warnings(comms.Cog):
 
         """
         self.bot = bot
+
+    async def cog_check(self, ctx: comms.Context) -> bool:
+        """Checks if user if owner.
+
+        Args:
+            ctx (:obj:`comms.Context`): Represents the context in which a command is being invoked under.
+
+        Returns:
+            True or false based off of if user is an owner of the bot.
+
+        """
+        return await self.bot.is_owner(ctx.author)
 
     """ Events """
 
@@ -90,8 +105,38 @@ class Warnings(comms.Cog):
                 )
 
             self.bot.log.info(f'Command has thrown error(s). They have been recorded. {error}')
+            tracebacker(error)
 
             await ctx.send(f'`Command has thrown error(s). They have been recorded.`')
+
+    @comms.command()
+    async def reports(self, ctx: comms.Context) -> None:
+        """ """
+        async with self.bot.pool.acquire() as conn:
+            r = await conn.fetch('''SELECT * FROM FailedCommands ORDER BY t ASC LIMIT 1''')
+
+            try:
+                d = {k: [v] for k, v in r[0].items() if k not in ['id']}
+
+            except IndexError:
+                return await ctx.send('`Records show that there are no reports as of now.`')
+
+            d['Time'] = [d.pop('t')[0].strftime('%c')]
+
+            desc = f'{fancy_embed(d, return_str=True)}\n{markdown_link("Jump", d.pop("jump")[0])}'
+            embed = discord.Embed(description=desc)
+
+            embed.set_footer(text='Trashcan removes the report from the database.')
+
+            msg = await ctx.send(embed=embed)
+            await msg.add_reaction('\U0001f5d1')
+
+            if await wait_for_reaction(ctx, '\U0001f5d1'):
+                await conn.execute(
+                    '''DELETE FROM FailedCommands WHERE identification = $1''',
+                    r[0]['identification']
+                )
+                await msg.delete()
 
 
 def setup(bot):
