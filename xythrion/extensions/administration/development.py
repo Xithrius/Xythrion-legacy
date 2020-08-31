8951f2615944e588d41628e7f182867ae209dd9e
Xithrius <xithrius@gmail.com>
2020-08-30 17:37:57 -0700

    Added sql command for fetching and executing.

--- xythrion/extensions/administration/development.py
+++ xythrion/extensions/administration/development.py
@@ -1,20 +1,15 @@
-"""
-> Xythrion: Graphing manipulated data through Discord.py.
-
-Copyright (c) 2020 Xithrius.
-MIT license, Refer to LICENSE for more info.
-"""
-
-
 from datetime import datetime
 from logging import getLogger
 from typing import Optional
 
-from discord import Embed
-from discord.ext.commands import Cog, command, Context, ExtensionNotLoaded, Greedy
 import humanize
+from discord import Embed, Message
+from discord.ext.commands import Cog, Context, ExtensionNotLoaded, Greedy, command
+from tabulate import tabulate
+
 from xythrion.bot import Xythrion
 from xythrion.extensions import EXTENSIONS
+from xythrion.utils import DefaultEmbed
 
 log = getLogger(__name__)
 
@@ -44,12 +39,13 @@ class Development(Cog):
             except Exception as e:
                 return log.warning(f'Reloading {extension} error: {e}')
 
-        log.info((
-            f'{ctx.author.name} reloaded extension{"" if ext else "s"} successfully. '
-            f'In about {humanize.naturaldelta(d - datetime.now(), minimum_unit="milliseconds")}ms'
-        ))
+        ms = humanize.naturaldelta(d - datetime.now(), minimum_unit="milliseconds")
 
-        await ctx.send(f'`Extension{" " + ext if ext else "s"} reloaded.`')
+        log.info(
+            f'{ctx.author.name} reloaded extension{"" if ext else "s"} successfully in about {ms}.'
+        )
+
+        await ctx.send(f'`Extension{" " + ext if ext else "s"} reloaded in about {ms}`')
 
     @command(name='logout', hidden=True)
     async def _logout(self, ctx: Context) -> None:
@@ -62,6 +58,8 @@ class Development(Cog):
         extensions = '\n'.join(f'{str(i).zfill(3)} | {ext}' for i, ext in enumerate(self.bot.cogs))
 
         embed = Embed(title='*Currently loaded cogs:*', description=f'```py\n{extensions}\n```')
+
+        embed = DefaultEmbed(description=f'```py\n{extensions}\n```')
 
         await ctx.send(embed=embed)
 
@@ -79,4 +77,25 @@ class Development(Cog):
 
     @command(name='raw')
     async def _raw(self, ctx: Context, msg: str) -> None:
+        """All information from a message object, if one can be found."""
         pass
+
+    @command(name='sql')
+    async def _sql(self, ctx: Context, command_type: str, *, _command: str) -> Optional[Message]:
+        """Executes a command given by the user. User has to be an owner."""
+        async with self.bot.pool.acquire() as conn:
+            if command_type == 'fetch':
+                output = await conn.fetch(_command)
+
+                keys = list(output[0].keys())
+                table = tabulate([[x[k] for k in keys] for x in output], showindex='Always', headers=keys)
+
+                await ctx.send(f'```sql\n{table}```')
+
+            elif command_type == 'execute':
+                output = await conn.execute(_command)
+                await ctx.send(f'```sql\n{output}```')
+
+            else:
+                embed = DefaultEmbed(description=f'"{command_type}" is not a valid command type.')
+                return await ctx.send(embed=embed)
