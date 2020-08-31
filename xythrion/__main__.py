abf2294be55605f490de5fd87c1d93f38393c2d1
Xithrius <xithrius@gmail.com>
2020-08-30 17:32:10 -0700

    Removed tmp folder creation.

--- xythrion/__main__.py
+++ xythrion/__main__.py
@@ -1,29 +1,16 @@
-"""
-> Xythrion: Graphing manipulated data through Discord.py.
-
-Copyright (c) 2020 Xithrius.
-MIT license, Refer to LICENSE for more info.
-"""
-
-
 import logging
-import os
-from pathlib import Path
 
 import discord
 from discord.ext.commands import when_mentioned_or
+
 from xythrion.bot import Xythrion
 from xythrion.extensions import EXTENSIONS
-
-from .constants import Config
-
+from .constants import BasicConfig, Config
 
 log = logging.getLogger(__name__)
 
-if not os.path.isdir(Path('tmp')):
-    os.mkdir(Path('tmp'))
-
 bot = Xythrion(
+    description=BasicConfig.BOT_DESCRIPTION,
     command_prefix=when_mentioned_or(';'),
     case_insensitive=True,
     help_command=None,
