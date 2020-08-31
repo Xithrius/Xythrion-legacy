9c6b3161cec792b54a7a8eb7a947f17d8cb10b8c
Xithrius <xithrius@gmail.com>
2020-08-30 17:34:55 -0700

    Added basic configuration for bot.

--- xythrion/constants.py
+++ xythrion/constants.py
@@ -1,16 +1,8 @@
-"""
-> Xythrion: Graphing manipulated data through Discord.py.
-
-Copyright (c) 2020 Xithrius.
-MIT license, Refer to LICENSE for more info.
-"""
-
-
 from os import environ
 from typing import NamedTuple
 
 __all__ = (
-    'Config', 'Postgresql'
+    'Config', 'Postgresql', 'BasicConfig'
 )
 
 
@@ -27,3 +19,9 @@ class Postgresql(NamedTuple):
     asyncpg_config = {'user': USER, 'password': PASSWORD, 'database': DATABASE, 'host': HOST}
     asyncpg_config_url = f'postgres://{USER}:{PASSWORD}@{HOST}:5432/{DATABASE}'
     asyncpg_default_docker_config = {'user': 'postgres', 'database': 'postgres', 'host': 'localhost'}
+
+
+class BasicConfig(NamedTuple):
+    BOT_ICON_LINK = environ.get(
+        'BOT_ICON_LINK', 'https://raw.githubusercontent.com/Xithrius/Xythrion/master/images/icon.png')
+    BOT_DESCRIPTION = environ.get('BOT_DESCRIPTION', 'Graphing manipulated data through discord.py')
