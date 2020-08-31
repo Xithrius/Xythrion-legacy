57d9eccbfa109bea2a7a08a540a752fb424248de
Xithrius <xithrius@gmail.com>
2020-08-30 17:22:09 -0700

    Subclassing discord.Embed to set defaults, removed parallel_executor.

--- xythrion/utils/shortcuts.py
+++ xythrion/utils/shortcuts.py
@@ -1,36 +1,21 @@
-"""
-> Xythrion: Graphing manipulated data through Discord.py.
-
-Copyright (c) 2020 Xithrius.
-MIT license, Refer to LICENSE for more info.
-"""
-
-
 import asyncio
-from datetime import datetime
-import functools
+import logging
 import os
-from pathlib import Path
 import typing as t
+from datetime import datetime
+from pathlib import Path
 
 from discord import Embed, Emoji, File, Member, Message, TextChannel
 from discord.ext.commands import BadArgument, Context, MessageConverter
+
+from xythrion.constants import BasicConfig
+
+log = logging.getLogger(__name__)
 
 
 def gen_filename() -> str:
     """Generates a filename from the current date."""
     return str(datetime.timestamp(datetime.now())).replace('.', '')
-
-
-def embed_attachment(p: str, embed: t.Optional[Embed] = None) -> t.Tuple[File, Embed]:
-    """Creating an embed and adding a local image to it."""
-    f = p.split(os.sep)[-1]
-    file = File(p, filename=f)
-    embed = Embed() if not embed else embed
-
-    embed.set_image(url=f'attachment://{f}')
-
-    return file, embed
 
 
 def shorten(s: str, approx_string_len: int = 10) -> str:
@@ -41,35 +26,9 @@ def shorten(s: str, approx_string_len: int = 10) -> str:
     return ' '.join(s[:approx_string_len + 1].split()[:-1]) + '...'
 
 
-def parallel_executor(func: t.Callable) -> t.Any:
-    """
-    Wrapper for making synchronous functions awaitable.
-
-    The synchronous function, after being turned into an asynchronous one, will only
-    return after the function is complete.
-    """
-    async def exec_func(sync_func: t.Callable, loop: asyncio.AbstractEventLoop) -> t.Any:
-        """Starting the executor and returning the response of the function."""
-        res = await loop.run_in_executor(None, sync_func)
-
-        return res
-
-    @functools.wraps(func)
-    async def wrapper(self: t.Generic, *args: t.Iterable[t.Any], **kwargs: t.Dict[t.Any, t.Any]) -> t.Any:
-        """The inner function, used for creating tasks so executors can be ran."""
-        if not isinstance(func, t.Callable):
-            raise ValueError('Incorrect function type passed. Did not get synchronous.')
-
-        func_partial = functools.partial(func, self, *args, **kwargs)
-        res = await exec_func(func_partial, self.bot.loop)
-
-        return res
-
-    return wrapper
-
-
 async def wait_for_reaction(ctx: Context, emoji: Emoji) -> bool:
     """Waiting for a user to react to a message sent by the bot."""
+
     def check(reaction: Emoji, user: Context.author) -> bool:
         return user == ctx.message.author and str(reaction.emoji) == emoji
 
@@ -120,3 +79,26 @@ async def get_discord_message(
 
     except BadArgument:
         return False
+
+
+class DefaultEmbed(Embed):
+    """Subclassing the embed class to set defaults."""
+
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+
+        self.set_footer(text=BasicConfig.BOT_DESCRIPTION, icon_url=BasicConfig.BOT_ICON_LINK)
+
+        if 'embed_attachment' in kwargs.keys():
+            v = kwargs['embed_attachment']
+            f = v.split(os.sep)[-1]
+            self.file = File(v, filename=f)
+
+            self.set_image(url=f'attachment://{f}')
+
+        try:
+            if '`' not in self.description or '\n' not in self.description:
+                self.description = f'`{self.description}`'
+
+        except TypeError:
+            pass
