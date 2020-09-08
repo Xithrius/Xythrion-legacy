46c848b40ff4e1732a934525ef71e5c21a99a618
Xithrius <xithrius@gmail.com>
2020-09-07 20:09:11 -0700

    Added API constants.

--- xythrion/constants.py
+++ xythrion/constants.py
@@ -2,7 +2,7 @@ from os import environ
 from typing import NamedTuple
 
 __all__ = (
-    'Config', 'Postgresql'
+    'Config', 'Postgresql', 'TwitterOAuth', 'WeatherAPIs',
 )
 
 
@@ -22,3 +22,15 @@ class Postgresql(NamedTuple):
     asyncpg_config = {'user': USER, 'password': PASSWORD, 'database': DATABASE, 'host': HOST}
     asyncpg_config_url = f'postgres://{USER}:{PASSWORD}@{HOST}:5432/{DATABASE}'
     asyncpg_default_docker_config = {'user': 'postgres', 'database': 'postgres', 'host': 'localhost'}
+
+
+class TwitterOAuth(NamedTuple):
+    CONSUMER_KEY = environ.get('TWITTER_CONSUMER_KEY')
+    CONSUMER_SECRET = environ.get('TWITTER_CONSUMER_SECRET')
+    ACCESS_TOKEN_KEY = environ.get('TWITTER_ACCESS_TOKEN_KEY')
+    ACCESS_TOKEN_SECRET = environ.get('TWITTER_ACCESS_TOKEN_SECRET')
+
+
+class WeatherAPIs(NamedTuple):
+    EARTH = environ.get('OPENWEATHERMAP_TOKEN')
+    MARS = environ.get('NASA_TOKEN')
