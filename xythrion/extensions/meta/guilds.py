4966ad8530671377ed676fcdc12f2cb3120d5dbf
Xithrius <xithrius@gmail.com>
2020-09-07 20:21:58 -0700

    Made user command better.

--- xythrion/extensions/meta/guilds.py
+++ xythrion/extensions/meta/guilds.py
@@ -1,10 +1,12 @@
-from typing import Optional
+from datetime import datetime
+from typing import Optional, Union
 
 import discord
 from discord.ext.commands import Cog, Context, command
+from humanize import naturaldelta
 
 from xythrion.bot import Xythrion
-from xythrion.utils import markdown_link
+from xythrion.utils import DefaultEmbed, markdown_link
 
 
 class Guilds(Cog):
@@ -21,6 +23,33 @@ class Guilds(Cog):
         embed = discord.Embed(description=markdown_link('icon url', mem.avatar_url))
 
         embed.set_image(url=mem.avatar_url)
+
+        await ctx.send(embed=embed)
+
+    @command(name='user', aliases=('userinfo',))
+    async def _user(self, ctx: Context, member: Optional[Union[discord.Member, int]] = None) -> None:
+        """Gets info of a user."""
+        if isinstance(member, int):
+            member = ctx.guild.get_member(member)
+
+        elif isinstance(member, discord.Member):
+            pass
+
+        else:
+            member = ctx.author
+
+        d = {
+            'User': [f'Username: {member.display_name}', f'Nickname: {member.nick}'],
+            'Created on': [
+                f'{member.created_at.strftime("%c")}, {naturaldelta(datetime.now() - member.created_at)}'],
+            'Joined on': [
+                f'{member.joined_at.strftime("%c")}, {naturaldelta(datetime.now() - member.joined_at)}']
+        }
+
+        formatted = '\n'.join([f'\n**{k}:**\n' + '\n'.join(x for x in v) for k, v in d.items()])
+        embed = DefaultEmbed(description=formatted)
+
+        embed.set_thumbnail(url=member.avatar_url)
 
         await ctx.send(embed=embed)
 
