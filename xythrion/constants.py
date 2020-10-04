997e92d50acd39e0ad2105ac28051063760e0645
Xithrius <xithrius@gmail.com>
2020-10-03 19:58:41 -0700

    Removed Twitter credential constants.

--- xythrion/constants.py
+++ xythrion/constants.py
@@ -1,15 +1,11 @@
 from os import environ
 from typing import NamedTuple
 
-__all__ = (
-    'Config', 'Postgresql', 'TwitterOAuth', 'WeatherAPIs',
-)
+__all__ = ('Config', 'Postgresql', 'WeatherAPIs')
 
 
 class Config(NamedTuple):
     TOKEN = environ.get('BOT_TOKEN')
-    BOT_ICON_LINK = environ.get(
-        'BOT_ICON_LINK', 'https://raw.githubusercontent.com/Xithrius/Xythrion/master/images/icon.png')
     BOT_DESCRIPTION = environ.get('BOT_DESCRIPTION', 'Graphing manipulated data through discord.py')
 
 
@@ -22,13 +18,6 @@ class Postgresql(NamedTuple):
     asyncpg_config = {'user': USER, 'password': PASSWORD, 'database': DATABASE, 'host': HOST}
     asyncpg_config_url = f'postgres://{USER}:{PASSWORD}@{HOST}:5432/{DATABASE}'
     asyncpg_default_docker_config = {'user': 'postgres', 'database': 'postgres', 'host': 'localhost'}
-
-
-class TwitterOAuth(NamedTuple):
-    CONSUMER_KEY = environ.get('TWITTER_CONSUMER_KEY')
-    CONSUMER_SECRET = environ.get('TWITTER_CONSUMER_SECRET')
-    ACCESS_TOKEN_KEY = environ.get('TWITTER_ACCESS_TOKEN_KEY')
-    ACCESS_TOKEN_SECRET = environ.get('TWITTER_ACCESS_TOKEN_SECRET')
 
 
 class WeatherAPIs(NamedTuple):
