094564589bb60c1d1b1dd9132ac432fcb129252b
Xithrius <xithrius@gmail.com>
2020-10-05 01:15:52 -0700

    Warnings now give an embed no matter what.

--- xythrion/extensions/administration/warnings.py
+++ xythrion/extensions/administration/warnings.py
@@ -1,7 +1,5 @@
 import logging
-from typing import Optional
 
-from discord import Message
 from discord.ext import commands
 from discord.ext.commands import Cog, Context
 
@@ -23,7 +21,7 @@ class Warnings(Cog):
         await ctx.message.add_reaction('\U00002705')
 
     @commands.Cog.listener()
-    async def on_command_error(self, ctx: Context, error: commands.CommandError) -> Optional[Message]:
+    async def on_command_error(self, ctx: Context, error: commands.CommandError) -> None:
         """When the command has an error, this event is triggered."""
         if hasattr(ctx.command, 'on_error'):
             return
@@ -32,27 +30,34 @@ class Warnings(Cog):
 
         await ctx.message.add_reaction('\U0000274c')
 
+        embed = DefaultEmbed(ctx, title='**An error has occurred:**')
+
         if isinstance(error, commands.DisabledCommand):
-            return await ctx.send('`Command not available.`')
+            embed.description = 'Command not available.'
 
         elif isinstance(error, commands.UserInputError):
-            return await ctx.send(f'`Command raised bad argument: {error}`')
+            embed.description = f'Command raised bad argument: {error}.'
 
         elif isinstance(error, commands.NotOwner):
-            return await ctx.send('`You do not have enough permissions for this command.`')
+            embed.description = 'You do not have enough permissions for this command.'
 
         elif isinstance(error, commands.CommandOnCooldown):
-            return await ctx.send(f'`{error}`')
+            embed.description = f'{error}.'
 
         elif isinstance(error, commands.CheckFailure):
-            return await ctx.send('`You do not have enough permissions to run this command.`')
+            embed.description = 'You do not have enough permissions to run this command.'
 
         elif isinstance(error, commands.MissingPermissions):
-            return await ctx.send('`Bot does not have enough permissions for this command.`')
+            embed.description = 'Bot does not have enough permissions for this command.'
 
-        embed = DefaultEmbed(ctx, title='**An error has occurred:**',
-                             description=f'{type(error).__name__}: {error}')
+        elif isinstance(error, commands.CommandNotFound):
+            embed.description = 'Unknown command.'
+
+        else:
+            embed.description = f'{type(error).__name__}: {error}'
+
+        embed.description = f'`{embed.description}`'
 
         await ctx.send(embed=embed)
 
-        log.warning(f'Error: {type(error).__name__}: {error}')
+        log.info(f'{type(error).__name__}: {error}')
