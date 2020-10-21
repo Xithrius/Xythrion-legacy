d8d5490d6d654243eca8b3144500d6f543a23869
Xithrius <xithrius@gmail.com>
2020-10-21 04:16:26 -0700

    Log folder will no longer be cleaned. Extensions are logged.

--- xythrion/__main__.py
+++ xythrion/__main__.py
@@ -1,28 +1,23 @@
 import logging
-from os import listdir, remove
-from pathlib import Path
 
-import discord
+from discord import AllowedMentions
 from discord.ext.commands import when_mentioned
 
 from xythrion.bot import Xythrion
+from xythrion.constants import Config
 from xythrion.extensions import EXTENSIONS
-from .constants import Config
 
 log = logging.getLogger(__name__)
-
-log.info('Cleaning out tmp/...')
-for file in listdir(Path.cwd() / 'tmp'):
-    remove(Path.cwd() / 'tmp' / file)
 
 bot = Xythrion(
     command_prefix=when_mentioned,
     case_insensitive=True,
     help_command=None,
-    allowed_mentions=discord.AllowedMentions(everyone=False)
+    allowed_mentions=AllowedMentions(everyone=False)
 )
 
 for extension in EXTENSIONS:
     bot.load_extension(extension)
+    log.info(f'Loaded extension "{extension}"')
 
 bot.run(Config.TOKEN, bot=True, reconnect=True)
