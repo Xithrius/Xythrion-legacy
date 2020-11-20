043cb56767c28f4d05664cfdd3754cd8752b5696
Xithrius <xithrius@gmail.com>
2020-11-19 20:37:41 -0800

    Added more extensions.

--- xythrion/extensions/requesters/__init__.py
+++ xythrion/extensions/requesters/__init__.py
@@ -1,5 +1,6 @@
 from xythrion.bot import Xythrion
 from xythrion.extensions.requesters.reddit import Reddit
+from xythrion.extensions.requesters.tinyy import Tinyy
 from xythrion.extensions.requesters.weather import Weather
 
 
@@ -7,3 +8,4 @@ def setup(bot: Xythrion) -> None:
     """The necessary function for loading in cogs within this folder."""
     bot.add_cog(Reddit(bot))
     bot.add_cog(Weather(bot))
+    bot.add_cog(Tinyy(bot))
