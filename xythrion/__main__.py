2c8331cc22a02fa1b8234faf580fbd9dc5508f1d
Xithrius <xithrius@gmail.com>
2020-11-19 20:26:45 -0800

    Changed prefix to '\'

--- xythrion/__main__.py
+++ xythrion/__main__.py
@@ -1,7 +1,6 @@
 import logging
 
 from discord import AllowedMentions
-from discord.ext.commands import when_mentioned
 
 from xythrion.bot import Xythrion
 from xythrion.constants import Config
@@ -10,10 +9,10 @@ from xythrion.extensions import EXTENSIONS
 log = logging.getLogger(__name__)
 
 bot = Xythrion(
-    command_prefix=when_mentioned,
+    command_prefix="\\",
     case_insensitive=True,
     help_command=None,
-    allowed_mentions=AllowedMentions(everyone=False)
+    allowed_mentions=AllowedMentions(everyone=False),
 )
 
 for extension in EXTENSIONS:
