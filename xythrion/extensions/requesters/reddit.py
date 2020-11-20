67090883e2eb58bcc7faa195741b8a55dbab0832
Xithrius <xithrius@gmail.com>
2020-11-19 20:32:50 -0800

    Formatting.

--- xythrion/extensions/requesters/reddit.py
+++ xythrion/extensions/requesters/reddit.py
@@ -16,35 +16,36 @@ class Reddit(Cog):
     @Cog.listener()
     async def on_message(self, message: Message) -> None:
         """Scans for Reddit posts and provides info on them."""
-        if 'https://www.reddit.com/r/' in message.content:
+        if "https://www.reddit.com/r/" in message.content:
             url = f'{message.content.rsplit("/", maxsplit=1)[0]}.json'
             async with self.bot.http_session.get(url) as resp:
                 assert resp.status == 200
                 _json = await resp.json()
-                _json = _json[0]['data']['children'][0]['data']
+                _json = _json[0]["data"]["children"][0]["data"]
 
-            if _json['over_18'] and not message.channel.is_nsfw():
+            if _json["over_18"] and not message.channel.is_nsfw():
                 return
 
             d = {
-                'Title': _json['title'],
-                'Subreddit': markdown_link(_json["subreddit"],
-                                           f'https://www.reddit.com/r/{_json["subreddit"]}'),
-                'Upvotes': _json['ups'],
-                'Upvotes/downvotes': f'{_json["upvote_ratio"] * 100}%',
-                'Image url': markdown_link('Link', _json['url'])
+                "Title": _json["title"],
+                "Subreddit": markdown_link(
+                    _json["subreddit"], f'https://www.reddit.com/r/{_json["subreddit"]}'
+                ),
+                "Upvotes": _json["ups"],
+                "Upvotes/downvotes": f'{_json["upvote_ratio"] * 100}%',
+                "Image url": markdown_link("Link", _json["url"]),
             }
-            formatted = '\n'.join(f'**{k}**: {v}' for k, v in d.items())
+            formatted = "\n".join(f"**{k}**: {v}" for k, v in d.items())
             embed = DefaultEmbed(self.bot, description=formatted)
 
             await message.channel.send(embed=embed)
 
-    @command(aliases=['sub', 'subreddit'])
-    async def reddit(self, ctx: Context, subreddit: str, status: str = 'hot', timeframe: str = 'day') -> None:
+    @command(aliases=["sub", "subreddit"])
+    async def reddit(self, ctx: Context, subreddit: str, status: str = "hot", timeframe: str = "day") -> None:
         """Requesting from the Reddit service to give a random post from a status within a timeframe."""
         status, timeframe = status.lower(), timeframe.lower()
-        statuses = ['top', 'hot', 'controversial', 'new', 'gilded']
-        timeframes = ['hour', 'day', 'week', 'month', 'year', 'all']
+        statuses = ["top", "hot", "controversial", "new", "gilded"]
+        timeframes = ["hour", "day", "week", "month", "year", "all"]
 
         if status not in statuses:
             await ctx.send(f'Please pick a status within `{", ".join(str(y) for y in statuses)}`')
@@ -52,34 +53,34 @@ class Reddit(Cog):
         if timeframe not in timeframes:
             await ctx.send(f'Please pick a timeframe within `{", ".join(str(y) for y in timeframes)}`')
 
-        url = f'https://reddit.com/r/{subreddit}/{status}.json?limit=100&t={timeframe}'
+        url = f"https://reddit.com/r/{subreddit}/{status}.json?limit=100&t={timeframe}"
 
         _json = await http_get(ctx, url)
 
-        _json = _json['data']['children']
-        p = _json[randint(0, len(_json) - 1)]['data']
+        _json = _json["data"]["children"]
+        p = _json[randint(0, len(_json) - 1)]["data"]
 
         fail = False
 
         try:
-            if p['over_18'] and not ctx.message.channel.is_nsfw():
+            if p["over_18"] and not ctx.message.channel.is_nsfw():
                 fail = True
 
         except AttributeError:
             fail = True
 
         if fail:
-            raise CheckFailure(message='NSFW')
+            raise CheckFailure(message="NSFW")
 
         image = False
 
-        if p['url'][-4:] in ('.jpg', 'jpeg', '.png'):
-            image = p['url']
+        if p["url"][-4:] in (".jpg", "jpeg", ".png"):
+            image = p["url"]
 
         desc = f'[`{shorten(p["title"])}`](https://reddit.com{p["permalink"]})'
-        embed = Embed(title=f'*r/{subreddit}*', description=desc)
+        embed = Embed(title=f"*r/{subreddit}*", description=desc)
 
         embed.set_footer(text=f'Upvotes: {p["ups"]}\nAuthor: u/{p["author"]}')
-        embed.set_image(url=image if image else '')
+        embed.set_image(url=image if image else "")
 
         await ctx.send(embed=embed)
--- xythrion/extensions/requesters/weather.py
+++ xythrion/extensions/requesters/weather.py
@@ -12,8 +12,8 @@ from xythrion.bot import Xythrion
 from xythrion.constants import WeatherAPIs
 from xythrion.utils import Graph, c2f, check_for_subcommands, http_get, k2c, k2f
 
-EARTH_URL = 'https://api.openweathermap.org/data/2.5/forecast?zip={0},{1}&appid={2}'
-MARS_URL = f'https://api.nasa.gov/insight_weather/?api_key={WeatherAPIs.MARS}&feedtype=json&ver=1.0'
+EARTH_URL = "https://api.openweathermap.org/data/2.5/forecast?zip={0},{1}&appid={2}"
+MARS_URL = f"https://api.nasa.gov/insight_weather/?api_key={WeatherAPIs.MARS}&feedtype=json&ver=1.0"
 
 
 class Weather(Cog):
@@ -29,23 +29,28 @@ class Weather(Cog):
             await check_for_subcommands(ctx)
 
     @weather.command()
-    async def earth(self, ctx: Context, zip_code: int, country_code: str = 'US') -> None:
+    async def earth(self, ctx: Context, zip_code: int, country_code: str = "US") -> None:
         """Getting weather for the planet of Earth."""
-        _json = await http_get(
-            ctx, EARTH_URL.format(zip_code, country_code.upper(), WeatherAPIs.EARTH))
+        _json = await http_get(ctx, EARTH_URL.format(zip_code, country_code.upper(), WeatherAPIs.EARTH))
 
         lst, dates = [], []
-        for i in _json['list']:
+        for i in _json["list"]:
             lst.append(
-                [k2f(i['main']['temp']), k2c(i['main']['temp']), i['main']['humidity'], i['wind']['speed']])
-            dates.append(datetime.fromtimestamp(i['dt']).strftime('%a: %H%p').lower().title())
+                [
+                    k2f(i["main"]["temp"]),
+                    k2c(i["main"]["temp"]),
+                    i["main"]["humidity"],
+                    i["wind"]["speed"],
+                ]
+            )
+            dates.append(datetime.fromtimestamp(i["dt"]).strftime("%a: %H%p").lower().title())
 
-        titles = ['°F', '°C', 'Humidity (%)', 'Wind (m/s)']
+        titles = ["°F", "°C", "Humidity (%)", "Wind (m/s)"]
 
-        func = functools.partial(self._create_weather_graph_and_table, ctx, lst, titles, dates, 'Time')
+        func = functools.partial(self._create_weather_graph_and_table, ctx, lst, titles, dates, "Time")
         _graph, _table = await self.bot.loop.run_in_executor(None, func)
 
-        _graph.embed.title = '**Weather on Earth.**'
+        _graph.embed.title = "**Weather on Earth.**"
 
         await ctx.send(file=_graph.embed.file, embed=_graph.embed, content=_table)
 
@@ -55,31 +60,36 @@ class Weather(Cog):
     async def mars(self, ctx: Context) -> None:
         """Getting weather for the planet of Mars."""
         _json = await http_get(ctx, MARS_URL)
-        sols = _json['sol_keys']
+        sols = _json["sol_keys"]
         lst = []
-        titles = ['°F', '°C', 'Pressure (Pa)', 'Wind (m/s)']
+        titles = ["°F", "°C", "Pressure (Pa)", "Wind (m/s)"]
 
         for sol in sols:
             try:
                 i = _json[sol]
-                c = i['AT']['av']
-                lst.append([c2f(c), c, i['PRE']['av'], i['HWS']['av']])
+                c = i["AT"]["av"]
+                lst.append([c2f(c), c, i["PRE"]["av"], i["HWS"]["av"]])
 
             except KeyError:
                 break
 
-        func = functools.partial(self._create_weather_graph_and_table, ctx, lst, titles, sols, 'Sol')
+        func = functools.partial(self._create_weather_graph_and_table, ctx, lst, titles, sols, "Sol")
         _graph, _table = await self.bot.loop.run_in_executor(None, func)
 
-        _graph.embed.title = f'**Weather on Mars sols {sols[0]}-{sols[-1]}.**'
+        _graph.embed.title = f"**Weather on Mars sols {sols[0]}-{sols[-1]}.**"
 
         await ctx.send(file=_graph.embed.file, embed=_graph.embed, content=_table)
 
         os.remove(_graph.save_path)
 
-    def _create_weather_graph_and_table(self, ctx: Context, data_lst: List[List[float]],
-                                        titles: List[str], days: List[str], day_title: str
-                                        ) -> Tuple[Graph, str]:
+    def _create_weather_graph_and_table(
+        self,
+        ctx: Context,
+        data_lst: List[List[float]],
+        titles: List[str],
+        days: List[str],
+        day_title: str,
+    ) -> Tuple[Graph, str]:
         """Manipulating JSON data from weather APIs."""
         fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(nrows=2, ncols=2)
         axes = [ax1, ax2, ax3, ax4]
@@ -99,10 +109,13 @@ class Weather(Cog):
     def _create_table(days: List[str], day_title: str, titles: List[str], lst: List[Any]) -> str:
         """Creates a table from the tabulate module."""
         table = tabulate(
-            [[days[i]] + x for i, x in enumerate(lst)], [day_title, *titles],
-            tablefmt='simple', showindex=False,
-            numalign='left', stralign='right',
-            floatfmt='.2f'
+            [[days[i]] + x for i, x in enumerate(lst)],
+            [day_title, *titles],
+            tablefmt="simple",
+            showindex=False,
+            numalign="left",
+            stralign="right",
+            floatfmt=".2f",
         )
 
-        return f'```py\n{table}```'
+        return f"```py\n{table}```"
