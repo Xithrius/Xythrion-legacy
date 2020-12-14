3eb3083d554ce1c3fb550fe464a7e0fd81ebdb6b
xithrius <xithrius@gmail.com>
2020-12-14 04:16:11 -0800

    Added barriers before DSL parsing starts.

--- xythrion/extensions/generation/graphing.py
+++ xythrion/extensions/generation/graphing.py
@@ -1,13 +1,14 @@
 import os
+import re
 from typing import List, Optional, Union
 
 from discord import Message
 from discord.ext.commands import Cog, Context, Greedy, group
 
 from xythrion.bot import Xythrion
-from xythrion.utils import DefaultEmbed, Graph, check_for_subcommands
+from xythrion.utils import DefaultEmbed, Graph, check_for_subcommands, remove_whitespace
 
-UNSUPPORTED_CHARACTERS = ("!", "{", "}", "[", "]")
+ILLEGAL_CHARACTERS = re.compile(r"[!{}\[\]]+")
 
 
 class Graphing(Cog):
@@ -16,40 +17,47 @@ class Graphing(Cog):
     def __init__(self, bot: Xythrion) -> None:
         self.bot = bot
 
-    @group(aliases=("plot",), enabled=False)
+    @staticmethod
+    def create_graph(
+        domain_nums: Optional[List[Union[int, float]]], x: List[Union[int, float]], y: List[Union[int, float]]
+    ) -> Graph:
+        """Creates a graph object after getting values within a domain from an expression."""
+        pass
+
+    @group(aliases=("plot",))
     async def graph(self, ctx: Context) -> None:
         """Group function for graphing."""
         if ctx.invoked_subcommand is None:
             await check_for_subcommands(ctx)
 
     @graph.command()
+    async def points(self, ctx: Context, points: remove_whitespace) -> None:
+        """
+        Graphs points on a plot.
+
+        Format: [(x0, y0), (x1, y1), (x2, y2),...] up to 100 points.
+        """
+        ...
+
+    @graph.command()
     async def expression(
-        self, ctx: Context, domain: Greedy[Union[int, float]], *, expression: str
+        self, ctx: Context, domain_numbers: Greedy[Union[int, float]], *, expression: remove_whitespace
     ) -> Optional[Message]:
         """
         Takes a single variable math expression and plots it.
 
-        Supports any single variable, e, and pi. Factorials are not supported.
+        Supports one variable per expression (ex. x or y, not x and y), e, and pi.
         """
+        if len(domain_numbers) not in (0, 2):
+            return await ctx.send(
+                f"There can be 2 or no domain integers/floats passed. {len(domain_numbers)} were passed."
+            )
 
-        def create_graph(ex: str, domain_nums: Optional[List[Union[int, float]]]) -> Graph:
-            """Creates a graph object after getting values within a domain from an expression."""
-            pass
-
-        if len(domain) != 2:
-            embed = DefaultEmbed(ctx, desc=f"Expected 2 domain numbers. Got {len(domain)}: {domain}")
+        if (illegal_char := re.search(ILLEGAL_CHARACTERS, expression)) is not None:
+            embed = DefaultEmbed(ctx, desc=f"Illegal character in expression: {illegal_char.group(0)}")
             return await ctx.send(embed=embed)
 
-        illegal_characters = [char for char in UNSUPPORTED_CHARACTERS if char in expression]
-
-        if any(illegal_characters):
-            chars = ", ".join(illegal_characters)
-            embed = DefaultEmbed(ctx, desc=f"Found unsupported characters in expression: {chars}")
-            await ctx.send()
-
-        domain = domain if len(domain) else None
-
-        graph = await self.bot.loop.run_in_executor(None, create_graph, expression)
+        graph = await self.bot.loop.run_in_executor(None, self.create_graph, expression)
 
         await ctx.send(file=graph.embed.file, embed=graph.embed)
 
