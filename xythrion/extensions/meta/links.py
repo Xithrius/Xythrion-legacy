bbb8e76ede718d807cffb3a08087ac79a51ca766
xithrius <xithrius@gmail.com>
2020-12-14 05:25:33 -0800

    Removed useless link commands.

--- xythrion/extensions/meta/links.py
+++ xythrion/extensions/meta/links.py
@@ -1,9 +1,10 @@
 from discord import Permissions
-from discord.ext.commands import Cog, Context, command, group
+from discord.ext.commands import Cog, Context, command
 from discord.utils import oauth_url
 
 from xythrion.bot import Xythrion
-from xythrion.utils import DefaultEmbed, check_for_subcommands, markdown_link
+from xythrion.constants import Config
+from xythrion.utils import DefaultEmbed, markdown_link
 
 
 class Links(Cog):
@@ -14,53 +15,15 @@ class Links(Cog):
 
     @command(aliases=("desc", "description"))
     async def info(self, ctx: Context) -> None:
-        """Information about bot origin along with usage statistics."""
-        info = {
-            "Xythrion Github repository": "https://github.com/Xithrius/Xythrion",
-            "Xithrius' Twitter": "https://twitter.com/_Xithrius",
-            "Xithrius' Github": "https://github.com/Xithrius",
-            "Xithrius' Twitch": "https://twitch.tv/Xithrius",
-        }
-
-        embed = DefaultEmbed(ctx, description="\n".join(markdown_link(k, v) for k, v in info.items()))
+        """Information about bot origin."""
+        embed = DefaultEmbed(ctx, description=markdown_link("Xythrion Github Repository", Config.GITHUB_URL))
 
         await ctx.send(embed=embed)
 
     @command()
     async def invite(self, ctx: Context) -> None:
         """Gives the invite link of this bot."""
-        url = oauth_url(self.bot.user.id, Permissions(19520))
+        url = oauth_url(self.bot.user.id, Permissions(3525696))
         embed = DefaultEmbed(ctx, description=markdown_link("invite_link", url))
 
         await ctx.send(embed=embed)
-
-    @group(aliases=("links",))
-    async def link(self, ctx: Context) -> None:
-        """Group command for management of links."""
-        if ctx.invoked_subcommand is None:
-            await check_for_subcommands(ctx)
-
-    @link.command()
-    async def show(self, ctx: Context, name: str) -> None:
-        """Shows a link that a user selected."""
-        pass
-
-    @link.command()
-    async def search(self, ctx: Context, name: str) -> None:
-        """Queries the database for a link."""
-        pass
-
-    @link.command()
-    async def random(self, ctx: Context) -> None:
-        """Selects a random link from the database."""
-        pass
-
-    @link.command()
-    async def add(self, ctx: Context, name: str, url: str) -> None:
-        """Adds a link to the database."""
-        pass
-
-    @link.command()
-    async def remove(self, ctx: Context, name: str) -> None:
-        """Removes a link from the database."""
-        pass
