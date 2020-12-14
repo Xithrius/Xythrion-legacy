8fffb268f1b3c3d88241efe74d545040eeda2364
xithrius <xithrius@gmail.com>
2020-12-14 05:25:55 -0800

    Added snippets cog for code blocks.

--- xythrion/extensions/meta/__init__.py
+++ xythrion/extensions/meta/__init__.py
@@ -1,9 +1,13 @@
 from xythrion.bot import Xythrion
+from xythrion.extensions.meta.dates import Dates
 from xythrion.extensions.meta.links import Links
-from xythrion.extensions.meta.source import Source
+from xythrion.extensions.meta.notes import Notes
+from xythrion.extensions.meta.snippets import Snippets
 
 
 def setup(bot: Xythrion) -> None:
     """The necessary function for loading in cogs within this folder."""
+    bot.add_cog(Dates(bot))
     bot.add_cog(Links(bot))
-    bot.add_cog(Source(bot))
+    bot.add_cog(Notes(bot))
+    bot.add_cog(Snippets(bot))
