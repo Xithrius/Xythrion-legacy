52a37aed53fb6ea0683d96e724e316275cd87554
xithrius <xithrius@gmail.com>
2020-12-19 19:46:32 -0800

    Obliterated the qrcode cog from existance.

--- xythrion/extensions/generation/__init__.py
+++ xythrion/extensions/generation/__init__.py
@@ -1,6 +1,5 @@
 from xythrion.bot import Xythrion
 from xythrion.extensions.generation.graphing import Graphing
-from xythrion.extensions.generation.qrcode import QRCode
 from xythrion.extensions.generation.randoms import Randoms
 from xythrion.extensions.generation.vectorization import Vectorization
 
@@ -8,6 +7,5 @@ from xythrion.extensions.generation.vectorization import Vectorization
 def setup(bot: Xythrion) -> None:
     """The necessary function for loading in cogs within this folder."""
     bot.add_cog(Graphing(bot))
-    bot.add_cog(QRCode(bot))
     bot.add_cog(Randoms(bot))
     bot.add_cog(Vectorization(bot))
