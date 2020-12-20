17b4e80a574884a7eacdf63b46b2f9f7447425a3
xithrius <xithrius@gmail.com>
2020-12-19 20:25:13 -0800

    Added Extension converter.

--- xythrion/utils/__init__.py
+++ xythrion/utils/__init__.py
@@ -1,4 +1,4 @@
-from .converters import remove_whitespace
+from .converters import Extension, remove_whitespace
 from .graphs import Graph
 from .shortcuts import DefaultEmbed, check_for_subcommands, gen_filename, http_get, markdown_link, shorten
 from .unit_conversion import c2f, c2k, k2c, k2f
@@ -16,4 +16,5 @@ __all__ = (
     "markdown_link",
     "shorten",
     "remove_whitespace",
+    "Extension",
 )
--- xythrion/utils/converters.py
+++ xythrion/utils/converters.py
@@ -1,5 +1,8 @@
 import re
 
+from discord.ext.commands import Context, Converter, UserInputError
+
+from xythrion.extensions import EXTENSIONS
 
 whitespace_pattern = re.compile(r"\s+")
 
@@ -7,3 +10,23 @@ whitespace_pattern = re.compile(r"\s+")
 def remove_whitespace(argument: str) -> str:
     """Replaces any whitespace within a string with nothingness."""
     return re.sub(whitespace_pattern, "", argument)
+
+
+class Extension(Converter):
+    """
+    Ensure the extension exists and return the full extension path.
+
+    The `*` symbol represents all extensions.
+    """
+
+    async def convert(self, ctx: Context, argument: str) -> str:
+        """Ensure the extension exists and return the full extension path."""
+        argument = argument.lower()
+
+        if "." not in argument:
+            argument = f"xythrion.extensions.{argument}"
+
+        if argument in EXTENSIONS:
+            return argument
+
+        raise UserInputError(f"Invalid argument {argument}")
