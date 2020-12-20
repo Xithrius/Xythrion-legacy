99d9a0baa83733ec06677e4ad31825f82bc59ba7
xithrius <xithrius@gmail.com>
2020-12-19 20:15:18 -0800

    Added the ability to reload specific extensions/cogs.

--- xythrion/extensions/administration/development.py
+++ xythrion/extensions/administration/development.py
@@ -7,7 +7,7 @@ from discord.ext.commands import Cog, Context, ExtensionNotLoaded, command, is_o
 
 from xythrion.bot import Xythrion
 from xythrion.extensions import EXTENSIONS
-from xythrion.utils import DefaultEmbed
+from xythrion.utils import DefaultEmbed, Extension
 
 log = getLogger(__name__)
 
@@ -20,11 +20,11 @@ class Development(Cog, command_attrs=dict(hidden=True)):
 
     @command(name="reload", aliases=("refresh", "r"))
     @is_owner()
-    async def _reload(self, ctx: Context, ext: Optional[str] = None) -> None:
-        """Reloads all extensions."""
+    async def reload(self, ctx: Context, *user_extensions: Optional[Extension]) -> None:
+        """Reloads either all extensions or specific ones given by the user."""
         d = datetime.now()
 
-        for extension in EXTENSIONS:
+        for extension in set(user_extensions or EXTENSIONS):
             try:
                 self.bot.reload_extension(extension)
 
@@ -32,15 +32,20 @@ class Development(Cog, command_attrs=dict(hidden=True)):
                 self.bot.load_extension(extension)
 
             except Exception as e:
-                return log.warning(f"Reloading {extension} error: {e}")
+                return log.error(f"Reloading {extension} error.", exc_info=(type(e), e, e.__traceback__))
 
         ms = humanize.naturaldelta(d - datetime.now(), minimum_unit="milliseconds")
 
-        log.info(f'{ctx.author.name} reloaded extension{"" if ext else "s"} successfully in about {ms}.')
+        if not user_extensions:
+            msg = (
+                f"Reloaded {len(self.bot.extensions)} extension(s) "
+                f"containing {len(self.bot.cogs)} cog(s) in in about {ms}."
+            )
+        else:
+            msg = f"Reloaded {len(user_extensions)} extension(s) in about {ms}."
 
-        embed = DefaultEmbed(
-            ctx,
-            description=f'Extension{" " + ext if ext else "s"} reloaded in about {ms}.',
-        )
+        log.info(msg)
+
+        embed = DefaultEmbed(ctx, description=msg)
 
         await ctx.send(embed=embed)
