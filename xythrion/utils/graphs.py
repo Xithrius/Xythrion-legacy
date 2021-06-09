b482539b99225d0abdba8c65dfe6c87eafc8475e
Xithrius <Xithrius@gmail.com>
2021-06-08 22:27:25 -0700

    Added graph_type argument for line/bar graphs.

--- xythrion/utils/graphs.py
+++ xythrion/utils/graphs.py
@@ -1,7 +1,7 @@
 import asyncio
 import functools
 from io import BytesIO
-from typing import Any, Callable
+from typing import Any, Callable, Union
 
 import numpy as np
 from loguru import logger as log
@@ -14,8 +14,8 @@ try:
 
     plt.style.use("dark_background")
 
-except (ImportError, ImportWarning) as error:
-    log.error("Error when importing Matplotlib.", exc_info=(type(error), error, error.__traceback__))
+except (ImportError, ImportWarning) as e:
+    log.error("Error when importing Matplotlib.", exc_info=(type(e), e, e.__traceback__))
 
 
 def plot_and_save(func: Callable) -> Any:
@@ -23,27 +23,42 @@ def plot_and_save(func: Callable) -> Any:
 
     @functools.wraps(func)
     async def wrapper(*args, **kwargs) -> Any:
-        return asyncio.get_event_loop().run_in_executor(func, *args, **kwargs)
+        sync_func = functools.partial(func, *args, **kwargs)
+
+        return await asyncio.get_event_loop().run_in_executor(None, sync_func)
 
     return wrapper
 
 
 @plot_and_save
-def graph_2d(x: np.ndarray, y: np.ndarray) -> BytesIO:
+def graph_2d(
+    x: Union[np.ndarray, list],
+    y: Union[np.ndarray, list],
+    *,
+    graph_type: str = "line",
+    autorotate_xaxis: bool = True
+) -> BytesIO:
     """Graphing points and saving said graph to a file."""
     buffer = BytesIO()
     fig, axis = plt.subplots()
 
-    fig.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
     axis.grid(True, linestyle="-.", linewidth=0.5)
 
-    axis.spines["left"].set_position("zero")
-    axis.spines["right"].set_color("none")
-    axis.spines["bottom"].set_position("zero")
-    axis.spines["top"].set_color("none")
+    if graph_type == "line":
+        axis.plot(x, y)
+    elif graph_type == "bar":
+        placements = np.linspace(0, len(x) + 1, len(x))
+        axis.bar(placements, y, 0.5)
+        axis.set_xticks(placements)
+        axis.set_xticklabels(x)
+    else:
+        raise ValueError(f"Invalid graph_type '{graph_type}'.")
 
-    axis.plot(x, y)
+    if autorotate_xaxis:
+        plt.gcf().autofmt_xdate()
 
     fig.savefig(buffer, format="png")
+
+    buffer.seek(0)
 
     return buffer
