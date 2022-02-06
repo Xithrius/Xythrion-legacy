d11c6862745008a8c34f2b7aa6dc878261f03f8d
Xithrius <xithrius@gmail.com>
2022-02-06 12:57:42 -0800

    All generation and requests will be moved to the rust webserver

--- xythrion/extensions/generation/__init__.py
+++
@@ -1,9 +0,0 @@
-from xythrion.bot import Xythrion
-from xythrion.extensions.generation.plotting import Plotting
-from xythrion.extensions.generation.randoms import Randoms
-
-
-def setup(bot: Xythrion) -> None:
-    """The necessary function for loading in cogs within this folder."""
-    bot.add_cog(Plotting(bot))
-    bot.add_cog(Randoms(bot))
--- xythrion/utils/__init__.py
+++ xythrion/utils/__init__.py
@@ -1,11 +1,9 @@
 from .converters import Extension, remove_whitespace
-from .graphs import graph_2d
 from .formatting import and_join, codeblock, markdown_link
 
 __all__ = (
     "Extension",
     "remove_whitespace",
-    "graph_2d",
     "and_join",
     "codeblock",
     "markdown_link"
