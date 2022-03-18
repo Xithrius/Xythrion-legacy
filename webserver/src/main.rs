6e5c6ebab5ab912233c1b5290beb2004d80a4ff8
Xithrius <xithrius@gmail.com>
2022-03-18 01:20:43 -0700

    Webserver can now accept requests

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Result;
+
+use actix_web::{get, App, HttpServer, Responder};
+
+#[get("/ping")]
+async fn ping() -> impl Responder {
+    "pong!".to_string()
+}
+
+#[actix_web::main]
+async fn main() -> Result<()> {
+    HttpServer::new(|| App::new().service(ping))
+        .bind(("127.0.0.1", 8090))
+        .unwrap()
+        .run()
+        .await
 }
